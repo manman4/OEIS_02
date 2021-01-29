86ecc1c2539ec1609b34a3eb937e350792b6f455
manman4 <manchanr4@gmail.com>
2021-01-30 02:12:38 +0900

    The exponential transform of sigma(n).

---
+++ 274/274804/274804_01.rb
@@ -0,0 +1,31 @@
+def sigma(n)
+  cnt = 0
+  (1..n).each{|i|
+    cnt += i if n % i == 0
+  }
+  cnt
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A340904(n)
+  s_ary = (0..n).map{|i| sigma(i)}
+  ary = [1]
+  (1..n).each{|i|
+    ary << (1..i).inject(0){|s, j| s + ncr(i - 1, j - 1) * s_ary[j] * ary[-j]}
+  }
+  ary
+end
+
+n = 22
+p ary = A340904(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
