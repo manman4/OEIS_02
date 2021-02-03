2c65b3602111181cfd90acb4c4179babd7406afa
manman4 <manchanr4@gmail.com>
2021-02-03 23:39:36 +0900

    a(n) = n! * [x^n] exp(n*x/(1 - x)).

---
+++ 293/293145/293145_01.rb
@@ -0,0 +1,23 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  return 1 if n == 0
+  (1..n).inject(0){|s, i| s + n ** i * ncr(n - 1, i - 1) * f(n) / f(i)}
+end
+
+n = 500
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
