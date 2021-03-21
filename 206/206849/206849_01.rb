48c1719b04bf0369603188071539691883f923f2
manman4 <manchanr4@gmail.com>
2021-03-22 01:01:08 +0900

    a(n) = Sum_{k=0..n} binomial(n^2, k^2).

---
+++ 206/206849/206849_01.rb
@@ -0,0 +1,20 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(n)
+  s = 0
+  (0..n).each{|i|
+    s += ncr(n * n, i * i)
+  }
+  s
+end
+
+n = 60
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
