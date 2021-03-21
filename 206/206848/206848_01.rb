190a9436df60a58341e6fd4de608b7c91b3149f0
manman4 <manchanr4@gmail.com>
2021-03-22 01:00:01 +0900

    G.f.: exp( Sum_{n>=1} x^n/n * Sum_{k=0..n} binomial(n^2, k^2) ).

---
+++ 206/206848/206848_01.rb
@@ -0,0 +1,28 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(n)
+  s = 0
+  (0..n).each{|i|
+    s += ncr(n * n, i * i)
+  }
+  s
+end
+
+def B(n)
+  ary = [1]
+  a = (0..n).map{|i| A(i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 70
+ary = B(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
