dd34cfd7d99abbd789a4450ba92762850fbe5fc8
manman4 <manchanr4@gmail.com>
2021-03-22 00:58:06 +0900

    G.f.: Product_{n>0} ( (1+x^n)/(1-x^n) )^(n^4).

---
+++ 206/206624/206624_01.rb
@@ -0,0 +1,22 @@
+def s(k, m, n)
+  s = 0
+  (1..n).each{|i| s += (n / i) ** m if n % i == 0 && i % k > 0}
+  s
+end
+
+def A(k, m, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(k, m, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s += k * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 3000
+ary = A(2, 5, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
