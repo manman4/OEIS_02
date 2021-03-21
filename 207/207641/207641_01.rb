3a904baa651c184a0c67e94b74421f20ef05a2f7
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-21 21:36:01 +0900

    G.f.: Sum_{n>=0} x^n * Product_{k=1..n} (1+x^k)/(1-x^k).

---
+++ 207/207641/207641_01.rb
@@ -0,0 +1,31 @@
+require 'prime'
+
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+def A207641(n)
+  a = A([[2, 1], [1, -2]], n)
+  ary = [1]
+  (1..n).each{|i| ary << a[i] - ary[i - 1]}
+  ary
+end
+
+n = 42
+p ary = A207641(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
