7995542438de4d9d03a04239af0153c0143238ba
manman4 <manchanr4@gmail.com>
2021-03-23 23:20:47 +0900

    Number of functions f:{1,2,...,2n}->{1,2,...,2n} such that every preimage has an even cardinality.

---
+++ 209/209289/209289_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  (0..2 * n).inject(0){|s, i| s + (n - i) ** (2 * n) * ncr(2 * n, i)}
+end
+
+n = 500
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
