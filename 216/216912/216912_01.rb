b48700877f70845529214b8c43bac455bb9f41c4
manman4 <manchanr4@gmai.com>
2021-05-01 02:56:31 +0900

    a(n) = denominator(B°(2*n))/4 where the B°(n) are Zagier's modified Bernoulli numbers.

---
+++ 216/216912/216912_01.rb
@@ -0,0 +1,31 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def A(n)
+  a = bernoulli(n)
+  a[1] = -1r / 2
+  (1..n).map{|i| (0..i).inject(0){|s, j| s + ncr(i + j, 2 * j) * a[j] / (i + j).to_r}}
+end
+
+n = 50
+ary = A(2 * n)
+(1..n).each{|i|
+  j = ary[2 * i - 1].denominator / 4
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
