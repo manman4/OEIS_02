b032bfa40812ba7c7fcfa62e558de6dc6198bef3
manman4 <manchanr4@gmai.com>
2021-05-01 02:52:20 +0900

    E.g.f. exp( x^2 * exp(x) ).

---
+++ 216/216507/216507_01.rb
@@ -0,0 +1,23 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i| ary << f(k) * (0..i - 1).inject(0){|s, j| s + ncr(i - 1, j) * ncr(j + 1, k) * ary[i - 1 - j]}}
+  ary
+end
+
+n = 20
+ary = A(2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
