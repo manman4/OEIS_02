2da3a36b3e84a90daba64212d9ff9e3713c68079
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-05 00:27:19 +0900

    Least number k > 1 such that (k^k-n^n)/(k-n) is an integer.

--- 242/242872/242872_01.rb
+++ 242/242872/242872_01.rb
@@ -1,6 +1,7 @@
 def A(n)
   return 3 if n == 2
   cnt = 2
+  # This is also the least number k such that (k^n-n^k)/(k-n) (= (n^k-k^n)/(n-k)) is an integer.
   while (n ** cnt - cnt ** n) % (n - cnt) > 0
     cnt += 1
   end
