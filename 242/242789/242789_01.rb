5afc164d54a12b7efaeaedce2b5a9ddc20635d9c
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-05 00:23:35 +0900

    Least number k > 1 such that (k^k-n)/(k-n) is an integer.

--- 242/242789/242789_01.rb
+++ 242/242789/242789_01.rb
@@ -1,7 +1,9 @@
 def A(n)
   return 3 if n == 2
   cnt = 2
-  # cnt - n < 0のときがあることに注意
+  # cnt - n < 0のときがあるので％の挙動に注意
+  # irb(main):001:0> 5%(-3)
+  # => -1
   while (cnt ** cnt - n) % (cnt - n) != 0
     cnt += 1
   end
