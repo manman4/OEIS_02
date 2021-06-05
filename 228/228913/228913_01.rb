fc36a801b79cf01d76148ed89a6a9eea6a8626cc
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-06 02:37:51 +0900

    a(n) = 11^n-10*10^n+45*9^n-120*8^n+210*7^n-252*6^n+210*5^n-120*4^n+45*3^n-10*2^n+1.

---
+++ 228/228913/228913_01.rb
@@ -0,0 +1,23 @@
+def A(k, n)
+  a = [1]
+  i = 1
+  i > k ? ary = [1] : ary = [0]
+  while ary.size < n + 1
+    a << 0
+    b = [1] + (1..i).map{|j| (j + 1) * a[j] - j * a[j - 1]}
+    a = b
+    i += 1
+    i > k ? ary << a[k] : ary << 0
+  end
+  ary[0..n]
+end
+
+n = 1000
+ary = A(10, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
