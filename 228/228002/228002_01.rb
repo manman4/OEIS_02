5dce5bcba6e29214a90c7105785fcfd119556405
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-06 02:29:26 +0900

    Alternate partial sums of binomial(2n,n)^2.

---
+++ 228/228002/228002_01.rb
@@ -0,0 +1,42 @@
+def ncr(n, r)
+  r = [r, n - r].min
+  return 1 if r == 0
+  return n if r == 1
+  numerator = (n - r + 1..n).to_a
+  denominator = (1..r).to_a
+  (2..r).each{|p|
+    pivot = denominator[p - 1]
+    if pivot > 1
+      offset = (n - r) % p
+      (p - 1).step(r - 1, p){|k|
+        numerator[k - offset] /= pivot
+        denominator[k] /= pivot
+      }
+    end
+  }
+  result = 1
+  (0..r - 1).each{|k|
+    result *= numerator[k] if numerator[k] > 1
+  }
+  return result
+end
+
+def A228002(n)
+  s = 0
+  ary = []
+  (0..n).each{|i|
+    s += (-1) ** (i % 2) * ncr(2 * i, i) ** 2
+    ary << (-1) ** (i % 2) * s
+  }
+  ary
+end
+
+n = 900
+ary = A228002(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
