6e5f14acf605ef056e0a386ce8ac8640cddb3659
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-06 21:14:41 +0900

    Expansion of f(x^1, x^6) in powers of x where f() is Ramanujan's general theta function.

---
+++ 274/274179/274179_01.rb
@@ -0,0 +1,27 @@
+def f(k, n)
+  ary = Array.new(n + 1, 0)
+  i = 0
+  while true
+    j = ((k + 1) * i * i - (k - 1) * i) / 2
+    break if j > n
+    ary[j] = 1
+    i += 1
+  end
+  i = 1
+  while true
+    j = ((k + 1) * i * i + (k - 1) * i) / 2
+    break if j > n
+    ary[j] = 1
+    i += 1
+  end
+  ary
+end
+
+k = 6
+n = 10000
+ary = f(k, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
