9d15427e1bda349d54cfba89de33a5d2dc6fa4c3
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-14 22:17:59 +0900

    Expansion of Product_{k>=2} (1 + x^k)^k.

---
+++ 298/298598/298598_01.rb
@@ -0,0 +1,28 @@
+def s(f_ary, g_ary, n, m)
+  s = 0
+  (m..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(ary, n, m = 1)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + s(j[0], j[1], i, m)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s + a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+m = 42
+n = m + 100
+ary1 = (0..n).map{|i| -i}
+ary2 = Array.new(n + 1, -1)
+
+# Product_{k>1} (1 + x^k)
+ary = A([[ary1, ary2]], n, 2)
+
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
