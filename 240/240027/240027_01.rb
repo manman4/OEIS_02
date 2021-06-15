a267805bd2da8e1af9c3f6bb80e681dfaa119df3
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-15 22:58:30 +0900

    Number of partitions of n such that the successive differences of consecutive parts are strictly increasing.

---
+++ 240/240027/240027_01.rb
@@ -0,0 +1,17 @@
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+def f(n)
+  return 1 if n == 0
+  cnt = 0
+  partition(n, 1, n).each{|ary|
+    ary0 = (1..ary.size - 1).map{|i| ary[i - 1] - ary[i]}
+    cnt += 1 if ary0.sort == ary0.reverse && ary0.uniq == ary0
+  }
+  cnt
+end
+def A240027(n)
+  (0..n).map{|i| f(i)}
+end
+p A240027(50)
