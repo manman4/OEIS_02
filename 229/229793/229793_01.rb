ec8dd95903d65f45fd57f9274ab8dbcf2a2936c3
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-15 22:43:25 +0900

    The expansion of R(q)^-5 in powers of q where R() is the Rogers-Ramanujan continued fraction.

---
+++ 229/229793/229793_01.rb
@@ -0,0 +1,20 @@
+def s(k, m, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == m}
+  s
+end
+
+def A(n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(5, 1, i) + s(5, 4, i) - s(5, 2, i) - s(5, 3, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + 5 * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 40
+ary = A(n + 1)
+-1.upto(n){|i|
+  print i
+  print ' '
+  puts ary[i + 1]
+}
