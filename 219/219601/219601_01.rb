a545005aeb6c0693c80188a6adeb4b177534a23a
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-16 21:59:07 +0900

    Number of partitions of n in which no parts are multiples of 6.

---
+++ 219/219601/219601_01.rb
@@ -0,0 +1,26 @@
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    s += i if n % i == 0 && i % k != 0
+  }
+  s
+end
+
+def B(k, n)
+  (1..n).map{|i| A(k, i)}
+end
+
+def C(k, n)
+  ary = [1]
+  a = [0] + B(k, n)
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 47
+ary = C(6, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
