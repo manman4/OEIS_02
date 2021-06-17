0e617f6c6a91053393e5f596f63afccbe12ab22b
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-17 22:53:39 +0900

    Expansion of 1/((1-x)(1-x^3)(1-x^5) ... (1-x^13)).

---
+++ 287/287997/287997_01.rb
@@ -0,0 +1,24 @@
+def T(n, k)
+  ary = []
+  1.step(2 * k, 2){|i| ary << i}
+  ps = Array.new(n + 1){0}
+  ps[0] = 1
+  ary.each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  ps[n]
+end
+
+def A(k, n)
+  (0..n).map{|i| T(i, k)}
+end
+
+n = 10000
+ary = A(7, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
