23ae85430027b5a25ba936a7fa684b05ced74255
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-20 00:40:41 +0900

    The characteristic function of square pyramidal numbers.

---
+++ 253/253903/253903_01.rb
@@ -0,0 +1,43 @@
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(k, n)
+  ary = Array.new(n + 1, 0)
+  i = 0
+  j = i * (i + 1) * (2 * i + 1) / 6
+  while j <= n
+    ary[j] += 1
+    i += 1
+    j = i * (i + 1) * (2 * i + 1) / 6
+  end
+  ary
+  power(ary, k, n)
+end
+
+n = 10000
+ary = A(1, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
