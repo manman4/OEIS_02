cdd971244a912f043a2aaf4ca88f51565ce55d70
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-22 23:20:10 +0900

    a(n) = Sum_{k=0..n} binomial(n+k+2,k)*binomial(2*n+1,n-k).

---
+++ 275/275827/275827_01.rb
@@ -0,0 +1,36 @@
+def ncr(n, r)
+  r = [r, n - r].min
+  return 1 if r == 0
+  return n if r == 1
+  numerator = (n - r + 1..n).to_a
+  denominator = (1..r).to_a
+  (2..r).each{|p|
+    pivot = denominator[p - 1]
+    if pivot > 1
+      offset = (n - r) % p
+      (p - 1).step(r - 1, p){|k|
+        numerator[k - offset] /= pivot
+        denominator[k] /= pivot
+      }
+    end
+  }
+  result = 1
+  (0..r - 1).each{|k|
+    result *= numerator[k] if numerator[k] > 1
+  }
+  return result
+end
+
+def A275827(n)
+  (0..n).map{|i| (0..i).inject(0){|s, j| s + ncr(i + j + 2, j) * ncr(2 * i + 1, i - j)}}
+end
+
+n = 19
+ary = A275827(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
