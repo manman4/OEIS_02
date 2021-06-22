c3e229a3e795b10ec54ff306ba53469561180034
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-22 23:16:31 +0900

    Expansion of Product_{k>=1} 1/(1 - x^k)^(sigma_2(k)).

---
+++ 275/275585/275585_02.rb
@@ -0,0 +1,42 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 10000
+ary1 = [0] + (1..n).map{|i| sigma(2, i)}
+ary2 = Array.new(n + 1, 1)
+ary = A(ary1, ary2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
