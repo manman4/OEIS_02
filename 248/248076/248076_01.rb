1a9a495bc7a209e71aa2d9d45d9906e53092d8bf
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-23 21:24:54 +0900

    Partial sums of the sum of the 5th powers of the divisors of n: Sum_{i=1..n} sigma_5(i).

---
+++ 248/248076/248076_01.rb
@@ -0,0 +1,27 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+n = 10000
+s = 0
+(1..n).each{|i|
+  s += sigma(5, i)
+  break if s.to_s.size > 1000
+  print i
+  print ' '
+  puts s
+}
