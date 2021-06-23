52e8f1e5233949a0e7adea37f1cdfc168639fc43
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-23 21:03:20 +0900

    Expansion of f(-x^8)^2 / f(-x) in powers of x where f() is a Ramanujan theta function.

---
+++ 260/260164/260164_01.rb
@@ -0,0 +1,69 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def p0(n)
+  (3 * n * n - n) / 2
+end
+
+def p1(n)
+  (3 * n * n + n) / 2
+end
+
+def A010815(n)
+  ary = Array.new(n + 1, 0)
+  ary[0] = 1
+  i = 1
+  j = p0(i)
+  while j <= n
+    ary[j] = (-1) ** i
+    i += 1
+    j = p0(i)
+  end
+  i = 1
+  j = p1(i)
+  while j <= n
+    ary[j] = (-1) ** i
+    i += 1
+    j = p1(i)
+  end
+  ary
+end
+
+def f(ary, k, n)
+  aryk = Array.new(n + 1, 0)
+  (0..n / k).each{|i| aryk[k * i] = ary[i]}
+  aryk
+end
+
+def A(k, n)
+  ary = A010815(n)
+  aryk = f(ary, k, n)
+  ps = Array.new(n + 1, 0)
+  ps[0] = 1
+  (1..n).each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  m_ary1 = mul(aryk, aryk, n)
+  mul(m_ary1, ps, n)
+end
+
+n = 1000
+ary = A(8, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
