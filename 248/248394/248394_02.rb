5dee1e358bf473a73bade2927776a84a95397f33
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-23 21:32:59 +0900

    q-Expansion of the modular form of weight 3/2, g*theta(2) in Tunnell's notation (see Comments).

---
+++ 248/248394/248394_02.rb
@@ -0,0 +1,34 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+n = 75
+ary0 = A([[2, 5], [8, 1], [1, -2], [4, -1]], n / 2)
+ary = Array.new(n + 1, 0)
+(0..(n - 1) / 2).each{|i| ary[2 * i + 1] = ary0[i]}
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
