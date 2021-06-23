d38073a0b30b292ebe7cf3d95eaec48b97b71eb1
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-23 21:25:48 +0900

    G.f.:  1 / AGM(1-33*x, sqrt((1-9*x)*(1-121*x))).

---
+++ 248/248167/248167_01.rb
@@ -0,0 +1,27 @@
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+# ary[0] = 1
+def sqrt_a(ary)
+  n = ary.size - 1
+  a = [1]
+  (0..n - 1).each{|i| a << (ary[i + 1] - (1..i).inject(0){|s, j| s + a[j] * a[-j]}) / 2}
+  a
+end
+
+n = 1000
+ary = sqrt_a(I([1, -14, 33] + [0] * (n - 1), n))
+(0..n).each{|i|
+  j = ary[i] ** 2
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
