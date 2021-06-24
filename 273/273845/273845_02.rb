6215cac4073a3923b1e81e28eb1ec7573966ed8d
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-24 22:41:48 +0900

    Expansion of Product_{n>=1} (1 - x^(3*n))/(1 - x^n)^3 in powers of x.

---
+++ 273/273845/273845_02.rb
@@ -0,0 +1,20 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += n / i if n % i == 0 && i % k > 0}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(k, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + k * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 36
+ary = A(3, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
