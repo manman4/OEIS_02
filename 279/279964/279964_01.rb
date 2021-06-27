cd57934c89f13e2dbe114e46bc7aea5a97e3425e
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-27 22:50:59 +0900

    a(n) = Sum_{k=1..n-1} sigma_3(k)*sigma_7(n-k).

---
+++ 279/279964/279964_01.rb
@@ -0,0 +1,30 @@
+require 'prime'
+
+def power(a, n)
+  return 1 if n == 0
+  k = power(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power(a, (n + 1) * x) - 1) / (power(a, x) - 1)}
+  sum
+end
+
+def A(k, n)
+  (1..n).map{|i| (1..i - 1).inject(0){|s, j| s + sigma(k, j) * sigma(k + 4, i - j)}}
+end
+
+n = 25
+ary = A(3, n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
