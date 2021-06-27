cbd7fb5d1a1d87805c305f37d07edec90508d34b
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-27 22:31:46 +0900

    Expansion of Product_{k>=1} (1 + x^(2*k)) / (1 - x^k).

---
+++ 279/279328/279328_01.rb
@@ -0,0 +1,48 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def f(n)
+  ary = []
+  1.step(n, 2){|i| ary << i}
+  ps = Array.new(n + 1){0}
+  ps[0] = 1
+  ary.each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  ps
+end
+
+def A(k, n)
+  ary = f(n)
+  aryk = Array.new(n + 1, 0)
+  (0..n / k).each{|i| aryk[k * i] = ary[i]}
+  ps = Array.new(n + 1){0}
+  ps[0] = 1
+  (1..n).each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  mul(aryk, ps, n)
+end
+
+n = 1000
+ary = A(2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
