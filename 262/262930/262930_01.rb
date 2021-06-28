0f1716e9ee670589bd9fdfac4952fa908cca282e
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-28 22:32:19 +0900

    Expansion of (psi(-q) / f(q^3))^2 in powers of q where psi(), f() are Ramanujan theta functions.

---
+++ 262/262930/262930_01.rb
@@ -0,0 +1,22 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s += j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+n = 46
+ary = A([[1, 2], [3, 2], [4, 2], [12, 2], [2, -2], [6, -6]], n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
