0f2a4ac1698a7c0a5add50f22ca434f8d1005a6d
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-29 22:49:41 +0900

    Number of Sylvester classes of 3-packed words of degree n.

---
+++ 243/243659/243659_02.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  (-1) ** (n % 2) * (0..n).inject(0){|s, i| s + (-2) ** i * ncr(n, i) * ncr(3 * n + i + 1, n) / (3 * n + i + 1r)}.to_i
+end
+
+
+n = 21
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
