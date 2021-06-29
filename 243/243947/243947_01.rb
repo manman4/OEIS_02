d45acad08894bef51e1ba720bdc3a0937f15241d
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-29 22:30:10 +0900

    Expansion of g.f. sqrt( (1+x - sqrt(1-18*x+x^2)) / (10*x*(1-18*x+x^2)) ).

---
+++ 243/243947/243947_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  (0..n).inject(0){|s, i| s + 5 ** (n - i) * ncr(2 * i, i) * ncr(2 * n + 1, 2 * i)}
+end
+
+n = 500
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
