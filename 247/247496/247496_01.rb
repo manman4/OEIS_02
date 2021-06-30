e23fe7abaf4fbc266979aee453b7c3b90cb82531
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-30 21:29:38 +0900

    a(n) = n!*[x^n](exp(n*x)*BesselI_{1}(2*x)/x), n>=0, main diagonal of A247495.

---
+++ 247/247496/247496_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  (0..n / 2).inject(0){|s, i| s + n ** (n - 2 * i) * ncr(n, 2 * i) * ncr(2 * i, i) / (i + 1)}
+end
+
+n = 500
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
