6447559e017741e381fc57dcadbb131bb4437822
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-30 23:29:14 +0900

    Number of 4 X n arrays with rows being permutations of 0..n-1 and no column j greater than column j-1 in all rows.

---
+++ 212/212857/212857_01.rb
@@ -0,0 +1,21 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n, k)
+  a = [1]
+  (1..n).each{|i| a << (0..i - 1).inject(0){|s, j| s + (-1) ** (j + i + 1) * ncr(i, j) ** k * a[j]}}
+  a
+end
+
+n = 200
+ary = A(n, 4)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
