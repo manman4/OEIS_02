526550a2a15d0b05401c84c98392f724ee71965e
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-01 22:15:47 +0900

    a(n) = 1 + 2*3 + 4*5*6 + 7*8*9*10 + ... + ...*n  (see Example lines).

---
+++ 227/227364/227364_01.py
@@ -0,0 +1,13 @@
+N = 34
+for n in range(N + 1):
+  sum = 0
+  i = k = 1
+  while i<=n:
+    product = 1
+    for x in range(k):
+      product *= i
+      i += 1
+      if i>n: break
+    sum += product
+    k += 1
+  print(str(n)+' '+str(sum))
