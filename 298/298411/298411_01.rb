1acb16b604be51c8f7512383cea082bd557274fa
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-05 01:41:48 +0900

    Coefficients of q^(-1/24)*eta(4q)^(1/2).

---
+++ 298/298411/298411_01.rb
@@ -0,0 +1,41 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def A(n)
+  ary = [1]
+  m = 1
+  (1..n).each{|i|
+    b_ary = Array.new(i + 1, 0)
+    m *= 4
+    b_ary[0], b_ary[-1] = 1, -m
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+# ary[0] = 1
+def sqrt_a(ary)
+  n = ary.size - 1
+  a = [1]
+  (0..n - 1).each{|i| a << (ary[i + 1] - (1..i).inject(0){|s, j| s + a[j] * a[-j]}) / 2}
+  a
+end
+
+n = 25
+ary = sqrt_a(A(n))
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
