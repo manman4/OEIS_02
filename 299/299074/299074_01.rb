3feb7cd62facc513102ab0c1307208f05c64dd8c
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-05 01:53:47 +0900

    Expansion of 1/((1-x)*(1-2*x)*(1-6*x)*(1-24*x)).

---
+++ 299/299074/299074_01.rb
@@ -0,0 +1,41 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+def A(n)
+  ary = [1]
+  a = [1]
+  s = 1
+  (1..n).each{|i|
+    s *= i
+    b_ary = [1, -s]
+    ary = mul(ary, b_ary, n)
+    p I(ary + [0] * (50 - i), 50)
+    a << I(ary, i)[i]
+  }
+  a
+end
+
+n = 4
+ary = A(n)
+# (0..n).each{|i|
+#   j = ary[i]
+#   break if j.to_s.size > 1000
+#   print i
+#   print ' '
+#   puts ary[i]
+# }
