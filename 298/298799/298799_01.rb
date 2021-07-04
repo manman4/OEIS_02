fd175aba25ac4482136cf2457704ad354280f2d5
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-05 02:01:09 +0900

    Expansion of (1-27*x)^(-1/9).

---
+++ 298/298799/298799_01.rb
@@ -0,0 +1,32 @@
+require 'prime'
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A003557(n)
+  s = 1
+  n.prime_division.each{|i| s *= i[0] ** (i[1] - 1)}
+  s
+end
+
+def A(a, k, n)
+  [1] + (1..n).map{|i| (k / a) ** i * (0..i - 1).inject(1){|s, j| s * (a * j - 1)} / f(i)}
+end
+
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+n = 17
+ary = I(A(9, 27, n), n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
