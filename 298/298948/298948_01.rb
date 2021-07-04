60b66d37c7f12df778ab47549939f126c7cd6979
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-05 02:02:25 +0900

    Expansion of Product_{k>=1} (1 - x^prime(k))^2.

---
+++ 298/298948/298948_01.rb
@@ -0,0 +1,40 @@
+require 'prime'
+
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def A(n)
+  ary = [1]
+  (1..n).each{|i|
+    if i.prime?
+      b_ary = Array.new(i + 1, 0)
+      b_ary[0], b_ary[-1] = 1, -1
+      ary = mul(ary, b_ary, n)
+    end
+  }
+  mul(ary, ary, n)
+end
+
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+n = 71
+p ary = A(n)
+p I(ary, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
