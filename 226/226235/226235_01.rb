60612af6ae7dc108d23848fc5a914bb42851d7f5
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-04 21:17:45 +0900

    Expansion of q * (chi(-q) / chi(-q^3))^12 in powers of q where chi() is a Ramanujan theta function.

---
+++ 226/226235/226235_01.rb
@@ -0,0 +1,20 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(2, i) + s(3, i) - s(1, i) - s(6, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + 12 * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 33
+ary = A(n - 1)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
