a1377ec850893dde023d3d37d2bfa5a9991442a5
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-07 22:04:38 +0900

    Number of integer solutions to a^2 + b^2 + c^2 + 7*d^2 = n.

---
+++ 236/236932/236932_01.rb
@@ -0,0 +1,26 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+def B(k, n)
+  A([[2, 15], [1, -6], [4, -6], [2 * k, 5], [k, -2], [4 * k, -2]], n)
+end
+
+n = 60
+ary = B(7, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
