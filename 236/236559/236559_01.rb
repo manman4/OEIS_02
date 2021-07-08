2627bcfbb674ff1a2890fc5d526d2bda971df8c4
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-08 23:53:25 +0900

    Number of partitions of 2n of type EO (see Comments).

---
+++ 236/236559/236559_01.rb
@@ -0,0 +1,28 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+def B(n)
+  a = A([[1, -1]], n)
+  b = A([[2, 2], [1, -1], [4, -1]], n)
+  (0..n).map{|i| (a[i] - b[i]) / 2}
+end
+
+n = 39
+ary = B(2 * n)
+(0..n).each{|i|
+  j = ary[2 * i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
