6c6b86ad6d26df51781bcc8c4fbc591f779b2437
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-10 00:12:40 +0900

    Odd primes p such that tau(p) is congruent to (p-1)/2 (mod p), where tau is the Ramanujan tau function (A000594).

---
+++ 296/296580/296580_01.rb
@@ -0,0 +1,36 @@
+require 'prime'
+
+def A296580(n)
+  ary = []
+  a = [0, 1]
+  (2..n).each{|i|
+    if i.prime?
+      s, t, u = 0, 1, 0
+      (1..n).each{|j|
+        t += 9 * j
+        u += j
+        break if i <= u
+        s += (-1) ** (j % 2 + 1) * (2 * j + 1) * (i - t) * a[-u]
+      }
+      v = s / (i - 1)
+      a << v
+      if v % i == i / 2
+        ary << i
+        p [i, v]
+      end
+    else
+      s = 1
+      i.prime_division.each{|j|
+        k, l = j[0], j[1]
+        if l == 1
+          s *= a[k]
+        else
+          s *= a[k ** (l - 1)] * a[k] - k ** 11 * a[k ** (l - 2)]
+        end
+      }
+      a << s
+    end
+  }
+  ary
+end
+p A296580(10 ** 5)
