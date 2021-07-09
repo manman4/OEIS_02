a7379d9cc79fa67650de16e2a29439e108ca8965
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-09 23:00:49 +0900

    a(n) = 1 + 2*n + 2^2*n*[n/2] + 2^3*n*[n/2]*[n/3] + 2^4*n*[n/2]*[n/3]*[n/4] + ... where [x]=floor(x).

---
+++ 208/208060/208060_01.rb
@@ -0,0 +1,19 @@
+def A(n)
+  s = 1
+  t = 1
+  (1..n).each{|i|
+    t *= 2 * (n / i)
+    s += t
+  }
+  s
+end
+
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
