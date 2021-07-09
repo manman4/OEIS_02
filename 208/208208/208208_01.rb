2c9894c6137fdb532cb290d50ba529db06b4ce7b
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-09 23:18:58 +0900

    a(n)=(a(n-1)^4*a(n-2)+1)/a(n-3) with a(0)=a(1)=a(2)=1.

---
+++ 208/208208/208208_01.rb
@@ -0,0 +1,21 @@
+def A(k, m, n)
+  a = Array.new(3, 1)
+  ary = [1]
+  while ary.size < n + 1
+    j = a[1] ** m * a[-1] ** k + 1
+    break if j % a[0] > 0
+    a = *a[1..-1], j / a[0]
+    ary << a[0]
+  end
+  ary
+end
+
+n = 12
+ary = A(4, 1, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
