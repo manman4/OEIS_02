d5c6e4f274d5d2175f052103fefadbaa01a99278
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-10 00:46:24 +0900

    Expansion of 1/Sum_{k>=0} A000326(k+1)*x^k.

---
+++ 296/296775/296775_01.rb
@@ -0,0 +1,16 @@
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+n = 30
+p ary0 = (1..n + 1).map{|i| i * (3 * i - 1) / 2}
+p ary = I(ary0, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
