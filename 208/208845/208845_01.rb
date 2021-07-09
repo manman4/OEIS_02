e8285adf78d1764b113e3f1b834b315d6b92391f
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-09 23:41:40 +0900

    Expansion of f(x)^2 in powers of x where f() is a Ramanujan theta function.

---
+++ 208/208845/208845_01.rb
@@ -0,0 +1,57 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def p0(n)
+  (3 * n * n - n) / 2
+end
+
+def p1(n)
+  (3 * n * n + n) / 2
+end
+
+def A010815(n)
+  ary = Array.new(n + 1, 0)
+  ary[0] = 1
+  i = 1
+  j = p0(i)
+  while j <= n
+    ary[j] = (-1) ** i
+    i += 1
+    j = p0(i)
+  end
+  i = 1
+  j = p1(i)
+  while j <= n
+    ary[j] = (-1) ** i
+    i += 1
+    j = p1(i)
+  end
+  ary
+end
+
+def A002107(n)
+  ary = A010815(n)
+  mul(ary, ary, n)
+end
+
+def A208845(n)
+  ary = A002107(n)
+  (0..n).map{|i| (-1) ** i * ary[i]}
+end
+
+n = 10000
+ary = A208845(n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
