5e7122f7fa79b3c6fb9bc0ebff0671381ecf8cce
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-15 22:54:16 +0900

    Somos's sequence {a(8,n)} defined in comment in A018896: a(0)=a(1)= ... = a(17) = 1; for n>=18, a(n) = (a(n-1)*a(n-17)+ a(n-9)^2)/a(n-18).

---
+++ 271/271838/271838_01.rb
@@ -0,0 +1,22 @@
+def A(k, n)
+  a = Array.new(2 * k, 1)
+  ary = [1]
+  while ary.size < n + 1
+    i = a[-1] * a[1] + a[k] ** 2
+    break if i % a[0] > 0
+    a = *a[1..-1], i / a[0]
+    ary << a[0]
+  end
+  ary
+end
+
+k = 9
+n = 600
+ary = A(k, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
