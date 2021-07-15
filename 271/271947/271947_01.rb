382f30a65b8e00c0c1c1bacc437a90647239d2a8
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-15 22:57:00 +0900

    a(n) = Product_{k=0..n} (6*k)!.

---
+++ 271/271947/271947_01.rb
@@ -0,0 +1,25 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+
+def A(n)
+  s = 1
+  ary = [1]
+  (1..n).each{|i|
+    s *= f(7 * i)
+    ary << s
+  }
+  ary
+end
+
+n = 30
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
