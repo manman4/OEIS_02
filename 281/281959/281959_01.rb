69ddbc8b8aa81e5c631258810e8edc1fff1814c9
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-20 00:14:46 +0900

    a(n) = sigma_25(n), the sum of the 25th powers of the divisors of n.

---
+++ 281/281959/281959_01.rb
@@ -0,0 +1,31 @@
+require 'prime'
+
+def power(a, n)
+  return 1 if n == 0
+  k = power(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power(a, (n + 1) * x) - 1) / (power(a, x) - 1)}
+  sum
+end
+
+def A(k, n)
+  (1..n).map{|i| sigma(k, i)}
+end
+
+n = 10000
+ary = A(25, n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
