5e2584a7f1949bf56e5525b031d42b38db572f3c
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-20 21:58:03 +0900

    10-adic integer x such that x^9 = 3.

---
+++ 225/225456/225456_01.rb
@@ -0,0 +1,30 @@
+# x‚©‚çŠJn‚µyæ‚µ‚Äp/q
+def A(x, y, p, q, base, n)
+  s = x
+  n.times{|i|
+    m = base ** (i + 1)
+    (0..base - 1).each{|j|
+      k = j * m + s
+      if (q * k ** y - p) % (m * base) == 0
+        s = k
+        break
+      end
+    }
+  }
+  s
+end
+
+def A225456(n)
+  str = A(3, 9, 3, 1, 10, n).to_s.reverse
+  (0..n).map{|i| str[i].to_i}
+end
+
+n = 10100
+ary = A225456(n)
+(0..10000).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
