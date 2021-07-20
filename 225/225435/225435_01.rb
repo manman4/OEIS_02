f7546a0775fc8aeac4200be7ed71a384e57268e2
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-20 21:36:44 +0900

    Numerators of convergents to the general continued fraction 1/(1 + 2/(1 + 3/(1 + 4/(1+ ...))))

---
+++ 225/225435/225435_01.rb
@@ -0,0 +1,18 @@
+def A(n)
+  s = n
+  (n - 1).downto(1){|i|
+    s += 1
+    s = i.to_r / s
+  }
+  s
+end
+
+n = 1000
+ary = (1..n).map{|i| A(i)}
+(1..n).each{|i|
+  j = ary[i - 1].numerator
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
