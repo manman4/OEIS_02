d69da5b36fdc920772252309836c87215ca00115
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 01:49:38 +0900

    Alternating binomial sums of the cubes of the central binomial coefficients.

---
+++ 276/276537/276537_01.rb
@@ -0,0 +1,36 @@
+def ncr(n, r)
+  r = [r, n - r].min
+  return 1 if r == 0
+  return n if r == 1
+  numerator = (n - r + 1..n).to_a
+  denominator = (1..r).to_a
+  (2..r).each{|p|
+    pivot = denominator[p - 1]
+    if pivot > 1
+      offset = (n - r) % p
+      (p - 1).step(r - 1, p){|k|
+        numerator[k - offset] /= pivot
+        denominator[k] /= pivot
+      }
+    end
+  }
+  result = 1
+  (0..r - 1).each{|k|
+    result *= numerator[k] if numerator[k] > 1
+  }
+  return result
+end
+
+def A(k, n)
+  (0..n).map{|i| (0..i).inject(0){|s, j| s + (-1) ** (i - j) * ncr(i, j) * ncr(2 * j, j) ** k}}
+end
+
+n = 60
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
