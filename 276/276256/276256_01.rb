2476cae61f21ac981f4e9510a31e693e4b879864
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 00:45:26 +0900

    A nonlinear recurrence of order 3: a(1)=a(2)=a(3)=1; a(n)=(a(n-1)+a(n-2)+1)^2/a(n-3).

---
+++ 276/276256/276256_01.rb
@@ -0,0 +1,25 @@
+def A(m, n)
+  a = Array.new(m, 1)
+  ary = [1]
+  while ary.size < n
+    i = a[1..-1].inject(:+) + 1
+    i *= i
+    break if i % a[0] > 0
+    a = *a[1..-1], i / a[0]
+    ary << a[0]
+  end
+  ary
+end
+def A276256(n)
+  A(3, n)
+end
+
+n = 18
+ary = A276256(n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
