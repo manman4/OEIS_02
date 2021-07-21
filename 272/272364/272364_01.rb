265ed92622a455cd70e5e28b58790e55817b2a73
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-21 23:41:51 +0900

    Number of integer solutions to a^2 + b^2 + 3*c^2 + 4*d^2 = n.

---
+++ 272/272364/272364_01.rb
@@ -0,0 +1,32 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+def B(a, n)
+  ary = []
+  a.each{|i|
+    ary << [2 * i, 5]
+    ary << [i, -2]
+    ary << [4 * i, -2]
+  }
+  A(ary, n)
+end
+
+n = 100
+p ary = B([1, 1, 3, 4], n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
