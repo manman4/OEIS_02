176c9d341c0e716400af3210df4512c6d4ff9e88
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 02:50:37 +0900

    Number of solutions of the congruence y^2  == x^3 - x^2 - 4*x + 4 (mod p) as p runs through the primes.

--- 276/276807/276807_01.rb
+++ 276/276807/276807_01.rb
@@ -5,7 +5,7 @@ def A(a3, a2, a4, a6, n)
   Prime.take(n).each{|p|
     a = Array.new(p, 0)
     (0..p - 1).each{|i| a[(i * i + a3 * i) % p] += 1}
-    ary << (0..p - 1).inject(0){|s, i| s += a[(i * i * i + a2 * i * i + a4 * i + a6) % p]}
+    ary << (0..p - 1).inject(0){|s, i| s + a[(i * i * i + a2 * i * i + a4 * i + a6) % p]}
   }
   ary
 end
