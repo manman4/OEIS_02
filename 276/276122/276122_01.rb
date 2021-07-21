a7118a695a614253c03ac0b3c1b1bea4640620bc
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 00:13:50 +0900

    a(0) = a(1) = a(2) = 1; for n > 2, a(n) = (a(n-1)^2+a(n-2)^2+a(n-1)+a(n-2))/a(n-3).

---
+++ 276/276122/276122_01.rb
@@ -0,0 +1,25 @@
+def A(m, n)
+  a = Array.new(m, 1)
+  ary = [1]
+  while ary.size < n + 1
+    i = a[1..-1].inject(0){|s, i| s + i * i} + a[1..-1].inject(:+)
+    break if i % a[0] > 0
+    a = *a[1..-1], i / a[0]
+    ary << a[0]
+  end
+  ary
+end
+
+def A276122(n)
+  A(3, n)
+end
+
+n = 20
+ary = A276122(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
