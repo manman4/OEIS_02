6de0c571dd0d1d381b15e3515c4aaa66379269d9
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 00:07:49 +0900

    Primes p such that A272207(p) = p.

---
+++ 276/276030/276030_01.rb
@@ -0,0 +1,29 @@
+require 'prime'
+def A(n)
+  ary = []
+  cnt = 1
+  Prime.each(2 * 10 ** 5).each{|p|
+    a = Array.new(p, 0)
+    (0..p - 1).each{|i| a[(i * i) % p] += 1}
+    s = 0
+    (0..p - 1).each{|i|
+      s += a[(i * i * i + i * i + 4 * i + 4) % p]
+      break if s > p
+    }
+    if p == s
+      ary << p
+      cnt += 1
+      return ary if cnt > n
+    end
+  }
+end
+
+n = 20
+p ary = A(n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
