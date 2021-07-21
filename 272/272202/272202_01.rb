0cec3ca4fbe285c7d47aa1fc94b5f8502386e864
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-21 23:37:56 +0900

    Number of solutions of the congruence y^2 == x^3 - 1 (mod p) as p runs through the primes.

---
+++ 272/272202/272202_01.rb
@@ -0,0 +1,21 @@
+require 'prime'
+
+def A(a3, a2, a4, a6, n)
+  ary = []
+  Prime.take(n).each{|p|
+    a = Array.new(p, 0)
+    (0..p - 1).each{|i| a[(i * i + a3 * i) % p] += 1}
+    ary << (0..p - 1).inject(0){|s, i| s + a[(i * i * i + a2 * i * i + a4 * i + a6) % p]}
+  }
+  ary
+end
+
+n = 100
+ary = A(0, 0, 0, -1, n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
