94c8c79fc8874c3a204a401081afb5aeb15e02c1
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 21:33:02 +0900

    Expansion of Product_{n>=1} (1 - x^(7*n))^7/(1 - x^n)^8 in powers of x.

---
+++ 277/277958/277958_01.rb
@@ -0,0 +1,89 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def p0(n)
+  (3 * n * n - n) / 2
+end
+
+def p1(n)
+  (3 * n * n + n) / 2
+end
+
+def A010815(n)
+  ary = Array.new(n + 1, 0)
+  ary[0] = 1
+  i = 1
+  j = p0(i)
+  while j <= n
+    ary[j] = (-1) ** i
+    i += 1
+    j = p0(i)
+  end
+  i = 1
+  j = p1(i)
+  while j <= n
+    ary[j] = (-1) ** i
+    i += 1
+    j = p1(i)
+  end
+  ary
+end
+
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A071734(n)
+  ary3 = power(A010815(n), 3, n)
+  ary73 = Array.new(n + 1, 0)
+  (0..n / 7).each{|i| ary73[7 * i] = ary3[i]}
+  ps = Array.new(n + 1){0}
+  ps[0] = 1
+  (1..n).each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  ary4 = power(ps, 4, n)
+  mul(ary73, ary4, n)
+end
+
+def A277958(n)
+  ary7 = power(A010815(n), 7, n)
+  ary77 = Array.new(n + 1, 0)
+  (0..n / 7).each{|i| ary77[7 * i] = ary7[i]}
+
+  ps = Array.new(n + 1){0}
+  ps[0] = 1
+  (1..n).each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  ary8 = power(ps, 8, n)
+  mul(ary77, ary8, n)
+end
+
+n = 1100
+ary = A277958(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
