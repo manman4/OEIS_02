39f1dd21232cf5b242bac45e77605aaf1bc87d97
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 16:14:28 +0900

    Positive integers n such that 3^n == 7 (mod n).

---
+++ 277/277126/277126_01.rb
@@ -0,0 +1,13 @@
+def pow(a, m, mod)
+  return 1 % mod if m == 0
+  k = pow(a, m >> 1, mod)
+  k *= k
+  return k % mod if m & 1 == 0
+  return k * a % mod
+end
+
+ary = []
+(1..10 ** 6).each{|i|
+  ary << i if pow(3, i, i) == (7 % i)
+}
+p ary
