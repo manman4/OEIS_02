24956058640a82fac11719f83fc0807e564c968c
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 23:41:50 +0900

    a(1) = 1, a(n+1) is the numerator of n-th partial fraction of the continued fraction [1; 4, 16, 64, ..., 4^n].

---
+++ 280/280220/280220_01.rb
@@ -0,0 +1,22 @@
+def A(m, n)
+  ary = []
+  (0..n - 1).each{|i|
+    s = m ** i
+    (i - 1).downto(0){|j|
+      s = 1r / s
+      s += m ** j
+    }
+    ary << s.numerator
+  }
+  ary
+end
+
+n = 100
+ary = A(4, n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
