1b4a2bfad28874059cd5dee539420dd26af5be51
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 22:24:23 +0900

    G.f.: Re((i; x)_inf), where (a; q)_inf is the q-Pochhammer symbol, i = sqrt(-1).

---
+++ 278/278399/278399_02.rb
@@ -0,0 +1,44 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# nŸ‚Ü‚Å
+def q(n)
+  return [1] if n == 0
+  ary = [1 - 1i]
+  (1..n).each{|i|
+    b_ary = Array.new(i + 1, 0)
+    b_ary[0], b_ary[-1] = 1, -1i
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+
+n = 56
+p ary = q(n)
+p ary.map{|i| (i / (1 - 1i)).real.to_i}
+p ary.map{|i| (i / (1 - 1i)).imaginary.to_i}
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i].real
+}
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i].imaginary
+}
