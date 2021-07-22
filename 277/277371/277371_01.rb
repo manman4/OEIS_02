50a04ef5c05d691eeb80711df994f22b7b0f3429
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 21:21:08 +0900

    Positive integers n such that n divides 7^n + 3.

---
+++ 277/277371/277371_01.rb
@@ -0,0 +1,18 @@
+def pow(a, m, mod)
+  return 1 if m == 0
+  k = pow(a, m >> 1, mod)
+  k *= k
+  return k % mod if m & 1 == 0
+  return k * a % mod
+end
+
+ary = []
+(1..3 * 10 ** 9).each{|k|
+  p k if k % (10 ** 8) == 0
+  l = pow(7, k, k)
+  if (l + 3) % k == 0
+    p k
+    ary << k
+  end
+  }
+p ary
