cd5a4df017c6f7dc2204201d1aecbc3d0d307c98
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 23:52:18 +0900

    a(n) = a(n-1) + 2^n * a(n-2) with a(0) = 1 and a(1) = 1.

---
+++ 280/280294/280294_01.rb
@@ -0,0 +1,20 @@
+def A(m, n)
+  a, b, i = 1, 1, 0
+  ary = [1]
+  while i < n
+    a, b = b, b + a * m ** (i + 2)
+    i += 1
+    ary << a
+  end
+  ary
+end
+
+n = 150
+ary = A(2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
