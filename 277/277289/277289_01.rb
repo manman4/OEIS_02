590c71c4e337d77525e236d87b207980f9694e15
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-22 17:27:55 +0900

    Positive integers n such that n | (3^n + 7).

---
+++ 277/277289/277289_01.rb
@@ -0,0 +1,13 @@
+def pow(a, m, mod)
+  return 1 % mod if m == 0
+  k = pow(a, m >> 1, mod)
+  k *= k
+  return k % mod if m & 1 == 0
+  return k * a % mod
+end
+
+ary = []
+(1..153872408).each{|i|
+  ary << i if pow(3, i, i) == (i - 7) % i
+}
+p ary
