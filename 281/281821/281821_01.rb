4b4f727b61592be0b269c45b25fab1e82331a826
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-26 23:49:42 +0900

    Denominator of Sum_{k=1..n} (30k-11)/(4*(2k-1)*k^3*binomial(2k,k)^2).

---
+++ 281/281821/281821_01.rb
@@ -0,0 +1,23 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(n)
+  s = 0
+  ary = []
+  (1..n).each{|i|
+    s += (30 * i - 11r) / ((8 * i - 4) * i * i * i * ncr(2 * i, i) ** 2)
+    ary << s
+  }
+  ary
+end
+
+n = 400
+ary = A(n)
+(1..n).each{|i|
+  j = ary[i - 1].denominator
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
