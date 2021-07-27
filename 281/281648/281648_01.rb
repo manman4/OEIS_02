48677d941bbba31aa36b7af96f81a29dbbc5c7d1
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-27 23:38:10 +0900

    (Numerator of Bernoulli(2*n)) read mod n.

---
+++ 281/281648/281648_01.rb
@@ -0,0 +1,24 @@
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0]
+  }
+  ary
+end
+def A281648(n)
+  a = bernoulli(2 * n)
+  (1..n).map{|i| a[2 * i].numerator % i}
+end
+
+n = 1000
+ary = A281648(n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
