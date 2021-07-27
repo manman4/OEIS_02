6c1d308692f03e1211385fde2bf757c8a4d57770
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-27 23:30:26 +0900

    Incrementally largest numerators of the Bernoulli numbers.

---
+++ 281/281386/281386_01.rb
@@ -0,0 +1,34 @@
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def A(n)
+  a = bernoulli(n)
+  ary = []
+  m = 0
+  (0..n).each{|i|
+    j = a[i].numerator
+    if j > m
+      ary << j
+      m = j
+    end
+  }
+  ary
+end
+
+n = 100
+ary = A(n)
+(1..ary.size).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
