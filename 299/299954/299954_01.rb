5c96497eb31098900f93844a284ce389b1e4da42
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-28 21:48:52 +0900

    Coefficients in expansion of 432 * (j^(1/2) + (j - 1728)^(1/2)) / (j^(1/2) - (j - 1728)^(1/2)), where j is the j-function.

---
+++ 299/299954/299954_01.rb
@@ -0,0 +1,124 @@
+require 'prime'
+
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def E_2k(k, n)
+  a = (-4 * k / bernoulli(2 * k)[-1])
+  b = a.denominator
+  c = a.numerator
+  [b] + (1..n).map{|i| c * sigma(2 * k - 1, i)}
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+def A008683(n)
+  ary = n.prime_division
+  return (-1) ** (ary.size % 2) if ary.all?{|i| i[1] == 1}
+  0
+end
+
+def A110163(n)
+  a = I(E_2k(2, n), n)
+  b = E_2k(3, n)
+  c = mul(a, b, n)
+  m_ary = [0] + (1..n).map{|i| A008683(i)}
+  ary = []
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += m_ary[i / j] * c[j] if i % j == 0
+    }
+    ary << 8 + s / (3 * i)
+  }
+  ary
+end
+
+def A288851(n)
+  a = I(E_2k(3, n), n)
+  b = E_2k(4, n)
+  c = mul(a, b, n)
+  m_ary = [0] + (1..n).map{|i| A008683(i)}
+  ary = []
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += m_ary[i / j] * c[j] if i % j == 0
+    }
+    ary << 12 + s / (2 * i)
+  }
+  ary
+end
+
+# m���ȉ������o��
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+n = 20
+a = A110163(n)
+b = A288851(n)
+p c = (0..n - 1).map{|i| 3 * a[i] - 2 * b[i]}
+d = [0] + c.map{|i| i / 288}
+ary = [1]
+(1..n).each{|i|
+  x = [1] + [0] * (i - 1) + [-1]
+  y = power(x, -d[i], n)
+  ary = mul(ary, y, n)
+}
+
+p a = power(ary, 144, n + 1)
+p (1..n + 1).map{|i| -a[i] % 864}
+p b = I((1..n + 1).map{|i| -a[i] / 864}, n)
+a[0] = 2
+p c = (0..n).map{|i| a[i] / 2}
+# A299954
+p mul(b, c, n)
