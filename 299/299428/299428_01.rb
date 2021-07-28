e6253a4f879b8c4b1d491009562758a88378bebd
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-28 22:37:44 +0900

    a(n) = binomial((n+1)*(2*n+1), n) * (n+1)/(2*n+1).

---
+++ 299/299428/299428_01.rb
@@ -0,0 +1,13 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+n = 1000
+(0..n).each{|i|
+  j = ncr((i + 1) * (2 * i + 1), i) * (i + 1) / (2 * i + 1)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
