0e3d204e1768080166b7c17abac5489fd50968da
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 00:12:37 +0900

    a(n) = A000594(n) mod (n+1).

---
+++ 299/299163/299163_01.rb
@@ -0,0 +1,38 @@
+require 'prime'
+
+def A(n)
+  a = [0, 1]
+  (2..n).each{|i|
+    if i.prime?
+      s, t, u = 0, 1, 0
+      (1..n).each{|j|
+        t += 9 * j
+        u += j
+        break if i <= u
+        s += (-1) ** (j % 2 + 1) * (2 * j + 1) * (i - t) * a[-u]
+      }
+      v = s / (i - 1)
+      a << v
+    else
+      s = 1
+      i.prime_division.each{|j|
+        k, l = j[0], j[1]
+        if l == 1
+          s *= a[k]
+        else
+          s *= a[k ** (l - 1)] * a[k] - k ** 11 * a[k ** (l - 2)]
+        end
+      }
+      a << s
+    end
+  }
+  a
+end
+
+n = 1000
+ary = A(n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i] % (i + 1)
+}
