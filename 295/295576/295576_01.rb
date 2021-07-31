2a1ca5b5db37debb5bba527fcfbe04c36a7bcf9d
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 16:11:19 +0900

    a(n) = Sum_{1 <= j <= n/2, gcd(j,n)=1} j^4.

---
+++ 295/295576/295576_01.rb
@@ -0,0 +1,14 @@
+def A(k, n)
+  s = 0
+  (1..n / 2).each{|i| s += i ** k if i.gcd(n) == 1}
+  s
+end
+
+n = 41
+(1..n).each{|i|
+  j = A(4, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
