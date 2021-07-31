99119a4848eb851850ff8c9bfb2f511c983bc08b
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 14:43:49 +0900

    Numerators of (-1)^(n+1) * (2*n)! * (2^(2*n)+1)/(B_{2*n} * 2^(4*n-1)), where B_{n} is the Bernoulli number.

---
+++ 295/295231/295231_01.rb
@@ -0,0 +1,26 @@
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+n = 13
+ary = bernoulli(2 * n)
+f_ary = (0..n).map{|i| (-1) ** (i % 2 + 1) * f(2 * i) * (2 ** (2 * i) + 1) / (ary[2 * i] * 2 ** (4 * i - 1))}
+(0..n).each{|i|
+  j = f_ary[i].numerator
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
