f902d82e7e4fcb130df977121c679a337b939291
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 21:56:32 +0900

    Expansion of Product_{k>=1} (1 - k*x^k)^(k^(2*k)).

---
+++ 294/294607/294607_01.rb
@@ -0,0 +1,24 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i ** (k * i + 1 + n / i) if n % i == 0}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  s_ary = [0] + (1..n).map{|i| s(k, i)}
+  (1..n).each{|i|
+    ary << -(1..i).inject(0){|s, j| s + s_ary[j] * ary[-j]} / i
+  }
+  ary
+end
+
+n = 50
+ary = A(2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
