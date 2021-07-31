b83bfdedbc9949b143d0861189950c119bd6ac29
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 20:23:30 +0900

    Square array A(n,k), n >= 0, k >= 0, read by antidiagonals, where column k is the expansion of e.g.f.: exp(1/(1-x)^k - 1).

---
+++ 294/294046/294046_01.rb
@@ -0,0 +1,36 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << k * (f(i - 1) * (1..i).inject(0){|s, j| s + ncr(j + k - 1, k) * ary[i - j] / f(i - j).to_r}).to_i
+  }
+  ary
+end
+def A294046(n)
+  a = []
+  (0..n).each{|i| a << A(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+n = 10
+ary = A294046(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
