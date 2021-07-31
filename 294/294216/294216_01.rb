bd20a463fbcc4c8167d31dbf5e61926932f80277
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 23:37:47 +0900

    E.g.f.: exp(1/((1-x)*(1-x^2)*(1-x^3)*(1-x^4)*(1-x^5)) - 1).

---
+++ 294/294216/294216_01.rb
@@ -0,0 +1,35 @@
+def A(k, n)
+  ary = (1..k).to_a
+  ps = Array.new(n + 1, 0)
+  ps[0] = 1
+  ary.each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  ps
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def B(k, n)
+  p_ary = A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (f(i - 1) * (1..i).inject(0){|s, j| s + j * p_ary[j] * ary[i - j] / f(i - j).to_r}).to_i
+  }
+  ary
+end
+
+n = 20
+ary = B(5, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
