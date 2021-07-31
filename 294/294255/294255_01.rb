6172d8b958f241c73b51e07542d8796baa3c1ec5
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 23:29:04 +0900

    E.g.f.: exp((1-x)*(1-x^2) - 1).

---
+++ 294/294255/294255_01.rb
@@ -0,0 +1,45 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  ary = [1] + Array.new(n, 0)
+  (1..k).each{|i|
+    b_ary = Array.new(i + 1, 0)
+    b_ary[0], b_ary[-1] = 1, -1
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+def B(k, n)
+  p_ary = A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (f(i - 1) * (1..[i, k * (k + 1) / 2].min).inject(0){|s, j| s + j * p_ary[j] * ary[i - j] / f(i - j).to_r}).to_i
+  }
+  ary
+end
+
+n = 22
+ary = B(2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
