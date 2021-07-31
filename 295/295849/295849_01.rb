498c09e61b72128e871bf60c7be85033a25168e7
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 19:41:55 +0900

    Number of nonnegative solutions to gcd(x,y,z) = 1 and x^2 + y^2 + z^2 <= n.

---
+++ 295/295849/295849_01.rb
@@ -0,0 +1,26 @@
+class Array
+  def gcd
+    self.inject{|a, b| a.gcd(b)}
+  end
+end
+
+def f(n)
+  s = 0
+  0.upto(n){|x|
+    0.upto(n){|y|
+      0.upto(n){|z|
+        if [x, y, z].gcd == 1 && x * x + y * y + z * z <= n
+          s += 1
+        end
+      }
+    }
+  }
+  s
+end
+
+n = 500
+(0..n).each{|i|
+  print i
+  print ' '
+  puts f(i)
+}
