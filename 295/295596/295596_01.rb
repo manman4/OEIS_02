fd2a0e37a77eaf785318339977270f3674b84710
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 16:47:41 +0900

    Numbers k such that Bernoulli number B_{k} has denominator 3404310.

---
+++ 295/295596/295596_01.rb
@@ -0,0 +1,27 @@
+require 'prime'
+
+def f(m, n)
+  cnt = 0
+  i = 2
+  while cnt < n
+    i += 1
+    ary = []
+    (1..Math.sqrt(i).to_i).each{|j|
+      if i % j == 0
+        ary << j
+        ary << i / j
+      end
+    }
+    ary.uniq!
+    s = 1
+    ary.each{|i| s *= i + 1 if (i + 1).prime?}
+    if s == m
+      cnt += 1
+      print cnt
+      print ' '
+      puts i
+    end
+  end
+end
+
+f(3404310, 1000)
