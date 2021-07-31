0e90f14cc1754ab633e8bda950374307c9ac0a7e
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 21:48:54 +0900

    Expansion of Product_{k>0} (1 - k^2*x^k)^(1/k).

---
+++ 294/294620/294620_01.rb
@@ -0,0 +1,29 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i ** (k * n / i) if n % i == 0}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  s_ary = [0] + (1..n).map{|i| s(k, i)}
+  (1..n).each{|i|
+    ary << -(1..i).inject(0){|s, j| s + s_ary[j] * ary[-j]} / i
+  }
+  ary
+end
+
+n = 35
+ary = A(2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
