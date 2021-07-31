0e9421ead6739eb8d8be55ec5b2815a88c1e3292
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 17:25:15 +0900

    Coefficients in expansion of E_4^(-1/4).

---
+++ 295/295815/295815_01.rb
@@ -0,0 +1,72 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def E_2k(k, n)
+  a = -4 * k / bernoulli(2 * k)[-1]
+  b = a.denominator
+  c = a.numerator
+  [b] + (1..n).map{|i| c * sigma(2 * k - 1, i)}
+end
+
+# ary[0] = 1, ary[1] �͋���
+def sqrt_a(ary)
+  n = ary.size - 1
+  a = [1, ary[1] / 2]
+  i = 1
+  while i < n
+    a << (ary[i + 1] - (1..i).inject(0){|s, j| s + a[j] * a[-j]}) / 2
+    i += 1
+  end
+  a
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+def E2k_m(k, m, n)
+  ary = E_2k(k, n)
+  power(ary, m, n)
+end
+
+n = 12
+ary = sqrt_a(sqrt_a(I(E_2k(2, n), n)))
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
