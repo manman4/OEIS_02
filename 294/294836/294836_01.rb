3a7a9eb8451fe216c1fc140da838c9107f2633b5
manman4 <7023566+manman4@users.noreply.github.com>
2021-07-31 21:24:43 +0900

    Expansion of Product_{k>=1} (1 + x^k)^(k*(2*k-1)).

---
+++ 294/294836/294836_01.rb
@@ -0,0 +1,25 @@
+def s(n)
+  s = 0
+  (1..n).each{|i| s += (-1) ** (n / i + 1) * i * i * (2 * i - 1) if n % i == 0}
+  s
+end
+
+def A(n)
+  ary = [1]
+  s_ary = [0] + (1..n).map{|i| s(i)}
+  (1..n).each{|i|
+    ary << (1..i).inject(0){|s, j| s + s_ary[j] * ary[-j]} / i
+  }
+  ary
+end
+
+n = 100
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
