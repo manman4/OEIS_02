c293c51071801787d2222cc2d99480a93de35b54
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 02:20:07 +0900

    Expansion of j(q) * q * Product_{n>=1} (1+q^n)^24 where j(q) is the elliptic modular invariant (A000521).

---
+++ 290/290271/290271_01.rb
@@ -0,0 +1,35 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+n = 14
+a = A([[2, 24], [1, -24]], n)
+a1 = [1] + a[0..-2].map{|i| 256 * i}
+a2 = mul(a1, a1, n)
+a3 = mul(a1, a2, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts a3[i]
+}
