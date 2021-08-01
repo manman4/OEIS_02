c7c308fa1df39471829eb1f4fd3320ab1787299d
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 01:58:41 +0900

    E.g.f.: exp(Sum_{k>=1} (k-1)^3*x^k).

---
+++ 290/290690/290690_01.rb
@@ -0,0 +1,20 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (f(i - 1) * (1..i).inject(0){|s, j| s + j * (j - 1) ** k * ary[i - j] / f(i - j).to_r}).to_i
+  }
+  ary
+end
+n = 20
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
