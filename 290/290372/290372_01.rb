ef2409c3a34550ca588eece8f3429411ce45e26d
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 02:23:54 +0900

    10-adic integer x = ...5807 satisfying x^5 = x.

---
+++ 290/290372/290372_01.rb
@@ -0,0 +1,37 @@
+def P(n)
+  s1, s2 = 2, 8
+  n.times{|i|
+    m = 10 ** (i + 1)
+    (0..9).each{|j|
+      k1, k2 = j * m + s1, (9 - j) * m + s2
+      if (k1 ** 5 - k1) % (m * 10) == 0 && (k2 ** 5 - k2) % (m * 10) == 0
+        s1, s2 = k1, k2
+        break
+      end
+    }
+  }
+  s1
+end
+def Q(s, n)
+  n.times{|i|
+    m = 10 ** (i + 1)
+    (0..9).each{|j|
+      k = j * m + s
+      if (k ** 2 - k) % (m * 10) == 0
+        s = k
+        break
+      end
+    }
+  }
+  s
+end
+def A290372(n)
+  str = (10 ** (n + 1) + P(n) - Q(5, n)).to_s.reverse
+  (0..n).map{|i| str[i].to_i}
+end
+p A290372(9999)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
