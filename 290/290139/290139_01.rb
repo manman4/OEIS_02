3495bdf9f1a60041883d6b13e9dfe1b827bbf74f
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-01 23:45:24 +0900

    a(n) = A225984(n) mod n.

---
+++ 290/290139/290139_01.rb
@@ -0,0 +1,17 @@
+def A225984(n)
+  a, b, c, d, e = 5, -1, 3, -7, 11
+  ary = [5]
+  n.times{
+    a, b, c, d, e = b, c, d, e, -e + d - c + a
+    ary << a
+  }
+  ary
+end
+
+n = 10000
+ary = A225984(n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i] % i
+}
