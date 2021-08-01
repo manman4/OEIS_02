4da3482ed349f7f295d37e3ef33d6883f2d4627b
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-01 23:04:38 +0900

    Square array A(n,k), n>=0, k>=0, read by antidiagonals, where column k is the expansion of (Sum_{j>=0} x^(j^3))^k.

---
+++ 290/290054/290054_01.rb
@@ -0,0 +1,54 @@
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def power(ary, n, m)
+  if n == 0
+    a = Array.new(m + 1, 0)
+    a[0] = 1
+    return a
+  end
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(k, n)
+  ary = Array.new(n + 1, 0)
+  i = 0
+  j = i * i * i
+  while j <= n
+    ary[j] += 1
+    i += 1
+    j = i * i * i
+  end
+  ary
+  power(ary, k, n)
+end
+
+n = 139
+a = []
+(0..n).each{|i| a << A(i, n - i)}
+ary = []
+(0..n).each{|i|
+  (0..i).each{|j|
+    ary << a[i - j][j]
+  }
+}
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
