e442f63d77bcea60b258d0471395eefe016537ba
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 00:57:53 +0900

    One of the two successive approximations up to 7^n for the 7-adic integer sqrt(-5). These are the numbers congruent to 4 mod 7 (except for the initial 0).

---
+++ 290/290809/290809_01.rb
@@ -0,0 +1,34 @@
+require 'OpenSSL'
+
+def f_a(ary, a)
+  (0..ary.size - 1).inject(0){|s, i| s + ary[i] * a ** i}
+end
+
+def df(ary)
+  (1..ary.size - 1).map{|i| i * ary[i]}
+end
+
+def A(c_ary, k, m, n)
+  x = OpenSSL::BN.new((-f_a(df(c_ary), k)).to_s).mod_inverse(m).to_i % m
+  f_ary = c_ary.map{|i| x * i}
+  f_ary[1] += 1
+  ary = [0]
+  a, mod = k, m
+  (n + 1).times{|i|
+    b = a % mod
+    ary << b
+    a = f_a(f_ary, b)
+    mod *= m
+  }
+  ary
+end
+
+n = 21
+ary = A([5, 0, 1], 4, 7, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
