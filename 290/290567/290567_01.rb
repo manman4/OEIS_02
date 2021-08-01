ee81d252daaf72f8e6d64231a9ee214bd5bc80ad
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 03:17:30 +0900

    The successive approximations up to 5^n for 5-adic integer 2^(1/3).

---
+++ 290/290567/290567_01.rb
@@ -0,0 +1,34 @@
+require 'OpenSSL'
+
+def f_a(ary, a)
+  (0..ary.size - 1).inject(0){|s, i| s + ary[i] * a ** i}
+end
+
+def df(ary)
+  (1..ary.size - 1).map{|i| i * ary[i]}
+end
+
+def A(c_ary, k, m, n)
+  x = OpenSSL::BN.new((-f_a(df(c_ary), k)).to_s).mod_inverse(m).to_i % m
+  f_ary = c_ary.map{|i| x * i}
+  f_ary[1] += 1
+  ary = [0]
+  a, mod = k, m
+  (n + 1).times{
+    b = a % mod
+    ary << b
+    a = f_a(f_ary, b)
+    mod *= m
+  }
+  ary
+end
+
+n = 24
+ary = A([-2, 0, 0, 1], 3, 5, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
