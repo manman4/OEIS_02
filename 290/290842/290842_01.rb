c5422effba8f9e2427081c649a4f9b09e40f24e9
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 00:56:06 +0900

    Numbers n such that sum of digits of n^3 is 3^3 = 27.

---
+++ 290/290842/290842_01.rb
@@ -0,0 +1,18 @@
+def A(k, n)
+  ary = []
+  i = 0
+  while ary.size < n
+    i += 1
+    j = (i * i * i).to_s.split('').map(&:to_i).inject(:+)
+    ary << i if j == k
+  end
+  ary
+end
+
+n = 500
+ary = A(27, n)
+(1..n).each{|i|
+  print i
+  print " "
+  puts ary[i - 1]
+}
