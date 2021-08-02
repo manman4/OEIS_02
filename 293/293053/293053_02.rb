3852f65ef6d3d25e45af2de0330bae0a72a542d0
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 15:41:42 +0900

    Square array A(n,k), n >= 0, k >= 0, read by antidiagonals, where column k is the expansion of e.g.f. Product_{i>k} exp(x^i).

---
+++ 293/293053/293053_02.rb
@@ -0,0 +1,25 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i| ary << (k..i - 1).inject(0){|s, j| s + f(j + 1) * ncr(i - 1, j) * ary[i - 1 - j]}}
+  ary
+end
+def A293053(n)
+  a = []
+  (0..n).each{|i| a << A(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+p A293053(20)
