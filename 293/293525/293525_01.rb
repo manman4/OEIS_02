042926f30755a21f2d347a7cf7ac56fac2c5efef
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 21:36:50 +0900

    Square array A(n,k), n >= 0, k >= 1, read by antidiagonals, where column k is the expansion of e.g.f. Product_{j > 0, j mod k > 0} exp(x^j).

---
+++ 293/293525/293525_01.rb
@@ -0,0 +1,62 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    b_ary = Array.new(n + 1, 0)
+    b_ary[0] = 1
+    (1..n).each{|j|
+      break if i * k * j > n
+      b_ary[i * k * j] = 1r / f(j)
+    }
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+def B(k, n)
+  ary = mul(A(1, n), I(A(k + 1, n), n), n)
+  (0..n).map{|j| (f(j) * ary[j]).to_i}
+end
+
+def A293525(n)
+  a = []
+  (0..n).each{|i| a << B(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+n = 10
+ary = A293525(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
