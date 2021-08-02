161bf98879541478854eafc37d277968562696e2
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 15:15:52 +0900

    Square array A(n,k), n >= 0, k >= 0, read by antidiagonals, where column k is the expansion of e.g.f.: exp(Sum_{j=1..k+1} binomial(k,j-1)*x^j/j).

---
+++ 293/293991/293991_01.rb
@@ -0,0 +1,38 @@
+def ncr(n, r)
+  return 1 if r == 0
+  return 0 if r > n
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+def A(k, n)
+  return Array.new(n + 1, 1) if k == 0
+  ary = [1]
+  (1..n).each{|i|
+    ary << (f(i - 1) * (1..i).inject(0){|s, j| s + ncr(k, j - 1) * ary[i - j] / f(i - j).to_r}).to_i
+  }
+  ary
+end
+def A293991(n)
+  a = []
+  (0..n).each{|i| a << A(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+n = 10
+ary = A293991(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
