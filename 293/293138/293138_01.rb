b43608f04d639b93f814142a1f6b283b64386dfd
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 16:21:22 +0900

    E.g.f.: Product_{m>0} (1+x^m+x^(2*m)/2!).

---
+++ 293/293138/293138_01.rb
@@ -0,0 +1,46 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    b_ary = Array.new(n + 1, 0)
+    b_ary[0] = 1
+    (1..k).each{|j|
+      break if i * j > n
+      b_ary[i * j] = 1r / f(j)
+    }
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+def B(k ,n)
+  return [1] + Array.new(n, 0) if k == 0
+  ary = A(k, n)
+  (0..n).map{|j| (f(j) * ary[j]).to_i}
+end
+
+n = 500
+ary = B(2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
