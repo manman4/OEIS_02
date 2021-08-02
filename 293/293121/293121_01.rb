fae0f1e5415e6fc23aee406e8b74568da04d7c96
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 16:05:16 +0900

    E.g.f.: exp(x^3/(1+x)).

---
+++ 293/293121/293121_01.rb
@@ -0,0 +1,22 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i| ary << (-1) ** (k % 2) * (k..i - 1).inject(0){|s, j| s + (-1) ** (j % 2) * f(j + 1) * ncr(i - 1, j) * ary[i - 1 - j]}}
+  ary
+end
+n = 22
+ary = A(2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
