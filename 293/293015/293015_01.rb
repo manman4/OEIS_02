7e71e464a246a85ad5c9794582c11376d6542dfe
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 15:11:52 +0900

    Square array A(n,k), n>=0, k>=0, read by antidiagonals, where A(0,k) = 1 and A(n,k) = - Sum_{i=0..n-1} binomial(n-1,i) * binomial(i+1,k) * A(n-1-i,k) for n > 0.

---
+++ 293/293015/293015_01.rb
@@ -0,0 +1,29 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i| ary << -(0..i - 1).inject(0){|s, j| s + ncr(i - 1, j) * ncr(j + 1, k) * ary[i - 1 - j]}}
+  ary
+end
+def A293015(n)
+  a = []
+  (0..n).each{|i| a << A(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+n = 10
+ary = A293015(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
