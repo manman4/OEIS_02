a114a460f766bc66596ae617109384c297990c22
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 13:36:00 +0900

    E.g.f.: exp(1 + x - exp(x)).

---
+++ 293/293037/293037_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i| ary << -(k..i - 1).inject(0){|s, j| s + ncr(i - 1, j) * ary[i - 1 - j]}}
+  ary
+end
+n = 20
+ary = A(1, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
