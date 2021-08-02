a24b7578e6a30fd67af6addb52fe5497f6cda67e
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 22:00:40 +0900

    E.g.f.: exp(Sum_{n>=1} A000041(n-1)*x^n/n).

---
+++ 293/293905/293905_01.rb
@@ -0,0 +1,32 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+def B(k, n)
+  p_ary = A([[1, -1]], n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (f(i - 1) * (1..i).inject(0){|s, j| s + j ** k * p_ary[j - 1] * ary[i - j] / f(i - j).to_r}).to_i
+  }
+  ary
+end
+n = 20
+ary = B(0, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
