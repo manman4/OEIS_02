eb5cb8d8e70e6c56bbff27e7df3e7d76a10641b1
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 21:10:01 +0900

    The PD_t(n) function (Number of tagged parts over all the partitions of n with designated summands).

---
+++ 293/293421/293421_02.rb
@@ -0,0 +1,23 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+n = 40
+ary1 = A([[3, 5], [1, -3], [6, -2]], n)
+ary2 = A([[6, 1], [1, -1], [2, -1], [3, -1]], n)
+(1..n).each{|i|
+  j = (ary1[i] - ary2[i]) / 2
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
