409b701d5e62a302eb9b8a82e423bcffbaf4458b
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 23:48:25 +0900

    Square array A(n,k), n >= 0, k >= 1, read by antidiagonals downwards, where column k is the expansion of e.g.f. exp(Sum_{j=1..k} x^j).

---
+++ 293/293669/293669_01.rb
@@ -0,0 +1,31 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (f(i - 1) * (1..[i, k + 1].min).inject(0){|s, j| s + j * ary[i - j] / f(i - j).to_r}).to_i
+  }
+  ary
+end
+def A293669(n)
+  a = []
+  (0..n).each{|i| a << A(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+n = 11
+ary = A293669(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
