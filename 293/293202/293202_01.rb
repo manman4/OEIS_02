15a12e70e1fff8f784015becff2308103f105523
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 16:31:32 +0900

    Square array A(n,k), n >= 0, k >= 0, read by antidiagonals, where column k is the expansion of g.f. Product_{i>0} Sum_{j=0..k} j!*x^(j*i).

---
+++ 293/293202/293202_01.rb
@@ -0,0 +1,57 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    b_ary = Array.new(n + 1, 0)
+    b_ary[0] = 1
+    (1..k).each{|j|
+      break if i * j > n
+      b_ary[i * j] = f(j)
+    }
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+def B(k ,n)
+  return [1] + Array.new(n, 0) if k == 0
+  ary = A(k, n)
+end
+
+def A293202(n)
+  a = []
+  (0..n).each{|i| a << B(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+
+n = 139
+ary = A293202(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
