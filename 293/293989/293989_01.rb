359327a2398a510a51431a8531a27d96d4ad4112
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-02 21:40:39 +0900

    a(0) = a(1) = 1 and a(n) = n! * [x^n] exp((1/(1-x)^(n-1) - 1)/(n-1)) for n > 1.

---
+++ 293/293989/293989_01.rb
@@ -0,0 +1,26 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+def A(k, n)
+  return Array.new(n + 1, 1) if k == 0
+  ary = [1]
+  (1..n).each{|i|
+    ary << (f(i - 1) * (1..i).inject(0){|s, j| s + ncr(j + k - 2, k - 1) * ary[i - j] / f(i - j).to_r}).to_i
+  }
+  ary
+end
+
+n = 17
+(0..n).each{|i|
+  j = A(i, i)[-1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
