6ed0631e483e7b521eeb410f6dfbb375c06092cd
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-03 21:35:09 +0900

    Square array A(n,k), n >= 0, k >= 0, read by antidiagonals, where column k is the expansion of g.f. Product_{i>0} 1/(1 + Sum_{j=1..k} (-1)^j*j*x^(j*i))^2.

---
+++ 292/292577/292577_01.rb
@@ -0,0 +1,63 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    b_ary = Array.new(n + 1, 0)
+    b_ary[0] = 1
+    (1..k).each{|j|
+      break if i * j > n
+      b_ary[i * j] = (-1) ** (j % 2) * j
+    }
+    ary = mul(ary, b_ary, n)
+  }
+  mul(ary, ary, n)
+end
+
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+def B(k, n)
+  return [1] + Array.new(n, 0) if k == 0
+  I(A(k, n), n)
+end
+
+def A292577(n)
+  a = []
+  (0..n).each{|i| a << B(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+
+n = 11
+ary = A292577(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
