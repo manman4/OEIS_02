3b788490978551ac6242304f18b93b009e210cfc
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-03 22:59:43 +0900

    E.g.f.: exp(1/5! * x^5 * exp(-x)).

---
+++ 292/292950/292950_01.rb
@@ -0,0 +1,19 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i| ary << (-1) ** (k % 2 + 1) * (0..i - 1).inject(0){|s, j| s + (-1) ** (j % 2) * ncr(i - 1, j) * ncr(j + 1, k) * ary[i - 1 - j]}}
+  ary
+end
+
+n = 25
+ary = A(5, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
