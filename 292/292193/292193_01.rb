7576740f0cbb21066dd100a02364304dee59beac
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-03 17:55:49 +0900

    Square array A(n,k), n >= 0, k >= 0, read by antidiagonals, where column k is the expansion of Product_{j>=1} 1/(1 - j^k*x^j).

---
+++ 292/292193/292193_01.rb
@@ -0,0 +1,49 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# n���܂�
+def q(k, n)
+  return [1] if n == 0
+  ary = [1]
+  (1..n).each{|i|
+    b_ary = Array.new(i + 1, 0)
+    b_ary[0], b_ary[-1] = 1, (-1) * i ** k
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+n = 10
+a = []
+(0..n).each{|i| a << I(q(i, n - i), n - i)}
+p a
+ary = []
+(0..n).each{|i|
+  (0..i).each{|j|
+    ary << a[i - j][j]
+  }
+}
+p ary
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+# p (0..n).map{|i| I(q(i, 2), 2)[-1]}
