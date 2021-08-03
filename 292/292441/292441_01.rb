03f0f6d83172e1bf8b090253276b871e82ddc8a6
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-03 21:12:19 +0900

    Largest m such that m^2 divides A000984(n).

---
+++ 292/292441/292441_01.rb
@@ -0,0 +1,24 @@
+require "prime"
+
+def f(n)
+  Prime.int_from_prime_division(
+    n.prime_division.map{|p,q| [p, q / 2]}
+  )
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+n = 100
+b=[]
+(0..n).each{|i|
+  j = f(ncr(2 * i, i))
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+b<<j
+}
+p b
