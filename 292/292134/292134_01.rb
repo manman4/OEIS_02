15f9eeb3fbe788a69eb7af4969d9670d5be10bde
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-03 16:19:50 +0900

    Main diagonal of A292133.

---
+++ 292/292134/292134_01.rb
@@ -0,0 +1,34 @@
+def A(k, n)
+  ary = [[1]]
+  (1..n).each{|i|
+    a = [0]
+    (1..((Math.sqrt(i * 8 + 1) - 1) / 2).floor).each{|j|
+      b = ary[i - j]
+      s = 0
+      s += k * b[j - 1].to_i + b[j].to_i
+      a << s
+    }
+    ary << a
+  }
+  ary
+end
+
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+def B(k, n)
+  I(A(k, n).map{|i| i.inject(:+)}, n)
+end
+
+n = 18
+ary = (0..n).map{|i| B(i, i)[-1]}
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
