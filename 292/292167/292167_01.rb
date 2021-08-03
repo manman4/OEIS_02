edd0dd6eae37be4dcea4255759ee4a894a4c1fa4
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-03 17:33:33 +0900

    Main diagonal of A292166.

---
+++ 292/292167/292167_01.rb
@@ -0,0 +1,33 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# nŸ‚Ü‚Å
+def q(k, n)
+  return [1] if n == 0
+  ary = [1]
+  (1..n).each{|i|
+    b_ary = Array.new(i + 1, 0)
+    b_ary[0], b_ary[-1] = 1, (-1) * i ** k
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+n = 120
+ary = (0..n).map{|i| q(i, i)[-1]}
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
