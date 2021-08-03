d340053ff530247a1038dbbd248b7c1795f34bd0
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-03 17:22:31 +0900

    a(n) = -A292140(n)/2.

---
+++ 292/292141/292141_01.rb
@@ -0,0 +1,24 @@
+def A(n)
+  ary = [[1]]
+  (1..n).each{|i|
+    a = [0]
+    (1..((Math.sqrt(i * 8 + 1) - 1) / 2).floor).each{|j|
+      b = ary[i - j]
+      s = 0
+      s += (-4) ** (1 - j % 2) * b[j - 1].to_i + b[j].to_i
+      a << s
+    }
+    ary << a
+  }
+  ary
+end
+
+n = 46
+ary = A(n).map{|i| i.inject(:+)}
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
