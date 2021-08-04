8f9a793a4b18720b707053f5f606a200f275e3c3
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-05 00:41:57 +0900

    Expansion of 1 - x/(1 - x^3/(1 - x^5/(1 - x^7/(1 - x^9/(1 - ... - x^(2*k-1)/(1 - ...)))))), a continued fraction.

---
+++ 291/291874/291874_02.rb
@@ -0,0 +1,25 @@
+def A(n)
+  ary = [[1]]
+  (1..n).each{|i|
+    a = [0]
+    (1..((Math.sqrt(i * 8 + 1) - 1) / 2).floor).each{|j|
+      b = ary[i - j]
+      s = 0
+      s += b[j - 1].to_i + b[j + 1].to_i
+      a << s
+    }
+    ary << a
+  }
+  ary
+end
+
+n = 52
+ary = A(n).map{|i| -(i[1].to_i)}
+ary[0] = 1
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
