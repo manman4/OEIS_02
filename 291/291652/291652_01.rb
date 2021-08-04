7e89d6adc0596f463fc97ae1e3facf6222fa8d9b
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-05 00:00:02 +0900

    Square array A(n,k), n>=0, k>=0, read by antidiagonals, where column k is the expansion of k-th power of continued fraction 1/(1 - x/(1 - x^2/(1 - x^3/(1 - x^4/(1 - x^5/(1 - ...)))))).

---
+++ 291/291652/291652_01.rb
@@ -0,0 +1,58 @@
+def A(n)
+  s = 1
+  f = [[1]]
+  ary = [1]
+  1.upto(n){|i|
+    s = 0
+    a = []
+    ((Math.sqrt(i * 8 + 1) - 1) / 2).floor.downto(1){|j|
+      a.unshift(s += f[i - j][j - 1])
+    }
+    f.push(a.unshift(s))
+    ary << s
+  }
+  ary
+end
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def power1(ary, n, m)
+  if n == 0
+    a = Array.new(m + 1, 0)
+    a[0] = 1
+    return a
+  end
+  k = power1(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+n = 139
+ary0 = A(n)
+a = []
+(0..n).each{|i| a << power1(ary0, i, n - i)}
+ary = []
+(0..n).each{|i|
+  (0..i).each{|j|
+    ary << a[i - j][j]
+  }
+}
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
