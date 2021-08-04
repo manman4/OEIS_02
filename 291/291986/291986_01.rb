113d80e3c04d15e3ee1512003ca545ea1602231d
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-04 12:47:25 +0900

    Expansion of 1/((1-x)*(1-2*x^2)*(1-3*x^3)).

---
+++ 291/291986/291986_01.rb
@@ -0,0 +1,31 @@
+# A291960
+def f(n)
+  ary = [[1]]
+  (1..n).each{|i|
+    a = [0]
+    (1..((Math.sqrt(i * 8 + 1) - 1) / 2).floor).each{|j|
+      b = ary[i - j]
+      s = 0
+      s += b[j - 1].to_i + j * b[j].to_i
+      a << s
+    }
+    ary << a
+  }
+  p ary
+end
+
+def A(k, n)
+  m = k * (k + 1) / 2
+  ary = f(n + m)
+  (m..m + n).map{|i| ary[i][k]}
+end
+
+n = 39
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
