7d42184ed2841e6dd8f748009341d6fd9f0834e1
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-05 00:51:22 +0900

    Triangle read by rows: T(n,k) = T(n-k,k-1) + T(n-k,k+1) with T(0,0) = 1 for 0 <= k <= A003056(n).

---
+++ 291/291904/291904_01.rb
@@ -0,0 +1,24 @@
+def A(n)
+  ary = [[1]]
+  (1..n).each{|i|
+    a = [0]
+    (1..((Math.sqrt(i * 8 + 1) - 1) / 2).floor).each{|j|
+      b = ary[i - j]
+      s = 0
+      s += b[j - 1].to_i + b[j + 1].to_i
+      a << s
+    }
+    ary << a
+  }
+  ary
+end
+
+n = 481
+ary = A(n).flatten
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
