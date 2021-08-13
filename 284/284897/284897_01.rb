b9e25cdbf6e6a7601a4ea1341b4bd4710703c59b
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-13 23:34:17 +0900

    Expansion of Product_{k>=1} 1/(1+x^k)^(k^3) in powers of x.

---
+++ 284/284897/284897_01.rb
@@ -0,0 +1,22 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += (-1) ** (n / i + 1) * i ** k if n % i == 0}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(k + 1, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 29
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
