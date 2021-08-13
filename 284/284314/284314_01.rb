6e9e7e05f67f8947ada0e9d494a21fd763176c7e
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-13 21:14:37 +0900

    Expansion of Product_{k>=0} (1 - x^(5*k+1)) in powers of x.

---
+++ 284/284314/284314_01.rb
@@ -0,0 +1,26 @@
+def A(k, m, n)
+  s = 0
+  (1..n).each{|i|
+    s += i if n % i == 0 && i % k == m
+  }
+  s
+end
+
+def B(k, m, n)
+  (1..n).map{|i| A(k, m, i)}
+end
+
+def C(k, m, n)
+  ary = [1]
+  a = [0] + B(k, m, n)
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 75
+ary = C(5, 1, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
