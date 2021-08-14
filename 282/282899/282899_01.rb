de54cf20598777ed4a0a73552b8ba9c550b85d05
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-14 21:30:06 +0900

    Denominators/48 of the coefficients of the series expansion of the Riemann-Siegel theta function at infinity.

---
+++ 282/282899/282899_01.rb
@@ -0,0 +1,20 @@
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+n = 1000
+ary = bernoulli(2 * n)
+(1..n).each{|i|
+  j = ((1 - 1r / 2 ** (2 * i - 1)) * ary[2 * i].abs / (4 * i * (2 * i - 1))).denominator / 48
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
