2b67b528eb79374d3e4173e9b83a7974f4b83000
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-14 17:18:16 +0900

    Sum_{d|n, d = 0, 1, or 5 mod 6} d.

---
+++ 284/284362/284362_01.rb
@@ -0,0 +1,14 @@
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    s += i if n % i == 0 && (i % k == 0 || i % k == 1 || i % k == k - 1)
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  print i
+  print ' '
+  puts A(6, i)
+}
