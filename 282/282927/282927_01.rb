1f5b42497728b836d02334c3533363b70baf4a5f
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-14 21:19:43 +0900

    Expansion of Product_{k>=1} (1 - x^(7*k))^36/(1 - x^k)^37 in powers of x.

---
+++ 282/282927/282927_01.rb
@@ -0,0 +1,71 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def p0(n)
+  (3 * n * n - n) / 2
+end
+
+def p1(n)
+  (3 * n * n + n) / 2
+end
+
+def A010815(n)
+  ary = Array.new(n + 1, 0)
+  ary[0] = 1
+  i = 1
+  j = p0(i)
+  while j <= n
+    ary[j] = (-1) ** i
+    i += 1
+    j = p0(i)
+  end
+  i = 1
+  j = p1(i)
+  while j <= n
+    ary[j] = (-1) ** i
+    i += 1
+    j = p1(i)
+  end
+  ary
+end
+
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(k, l, n)
+  aryk = power(A010815(n), k, n)
+  aryk7 = Array.new(n + 1, 0)
+  (0..n / 7).each{|i| aryk7[7 * i] = aryk[i]}
+  ps = Array.new(n + 1){0}
+  ps[0] = 1
+  (1..n).each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  aryl = power(ps, l, n)
+  mul(aryk7, aryl, n)
+end
+
+n = 1000
+ary = A(36, 37, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
