702e45b5d971fa0686eff773aef70bea72fda308
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-14 21:49:30 +0900

    Expansion of b(q) * b(q^3) in powers of q where b() is a cubic AGM function.

---
+++ 282/282610/282610_01.rb
@@ -0,0 +1,20 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+n = 68
+ary = A([[1, 3], [3, 2], [9, -1]], n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
