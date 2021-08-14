55576da5acaaf52157aa04226ca4e3bd56565e0c
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-14 21:59:41 +0900

    Coefficients in q-expansion of E_2*E_6^2, where E_2 and E_6 are respectively the Eisenstein series A006352 and A013973.

---
+++ 282/282576/282576_01.rb
@@ -0,0 +1,66 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def A(k, n)
+  a = (-4 * k / bernoulli(2 * k)[-1])
+  b = a.denominator
+  c = a.numerator
+  [b] + (1..n).map{|i| c * sigma(2 * k - 1, i)}
+end
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+n = 1100
+p_ary = A(1, n)
+r_ary = A(3, n)
+r2_ary = power(r_ary, 2, n)
+pr2_ary = mul(p_ary, r2_ary, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts pr2_ary[i]
+}
