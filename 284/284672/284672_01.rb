b84a7a7025d500d900c6ac15b94154e17dfa4dfd
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-14 17:53:32 +0900

    Exponents of powers of 7 that contain all ten decimal digits.

---
+++ 284/284672/284672_01.rb
@@ -0,0 +1,20 @@
+def A(k, n)
+  i = 0
+  s = 1
+  ary = []
+  while ary.size < n
+    i += 1
+    s *= k
+    str = s.to_s.split('')
+    ary << i if str.include?("0") && str.include?("1") && str.include?("2") && str.include?("3") && str.include?("4") && str.include?("5") && str.include?("6") && str.include?("7") && str.include?("8") && str.include?("9")
+  end
+  ary
+end
+
+n = 10000
+ary = A(7, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
