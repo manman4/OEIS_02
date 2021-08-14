6388acba02a9e94f082e98fb06f6bc55d7c94086
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-15 00:15:19 +0900

    Coefficients in q-expansion of E_4^3*E_6^3, where E_4 and E_6 are respectively the Eisenstein series A004009 and A013973.

---
+++ 282/282461/282461_01.rb
@@ -0,0 +1,83 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def A(k, n)
+  a = (-4 * k / bernoulli(2 * k)[-1])
+  b = a.denominator
+  c = a.numerator
+  [b] + (1..n).map{|i| c * sigma(2 * k - 1, i)}
+end
+
+# m次以下を取り出す
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# m次以下を取り出す
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A000594(n)
+  ary = Array.new(n + 1, 0)
+  # ヤコビの公式の必要なところだけ取り出す
+  i = 0
+  j, k = 2 * i + 1, i * (i + 1) / 2
+  while k <= n
+    i & 1 == 1? ary[k] = -j : ary[k] = j
+    i += 1
+    j, k = 2 * i + 1, i * (i + 1) / 2
+  end
+  # 8乗してx倍
+  power(ary, 8, n).unshift(0)[1..n]
+end
+
+n = 1000
+q_ary = A(2, n)
+q3_ary = power(q_ary, 3, n)
+r_ary = A(3, n)
+r3_ary = power(r_ary, 3, n)
+q3r3_ary = mul(q3_ary, r3_ary, n)
+(0..n).each{|i|
+  j = q3r3_ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
