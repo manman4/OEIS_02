3ba53ea26ca2a85fe6c4f5c06df15821dd231754
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-14 23:07:10 +0900

    Coefficients in q-expansion of E_2^3, where E_2 is the Eisenstein series shown in A006352.

---
+++ 282/282018/282018_01.rb
@@ -0,0 +1,57 @@
+require 'prime'
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def power(a, n)
+  return 1 if n == 0
+  k = power(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power(a, (n + 1) * x) - 1) / (power(a, x) - 1)}
+  sum
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def A(k, n)
+  a = (-4 * k / bernoulli(2 * k)[-1])
+  a = a.to_i if a.denominator == 1
+  [1] + (1..n).map{|i| a * sigma(2 * k - 1, i)}
+end
+
+n = 1100
+ary2 = A(1, n)
+ary2_2 = mul(ary2, ary2, n)
+ary2_3 = mul(ary2, ary2_2, n)
+(0..n).each{|i|
+  j = ary2_3[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
