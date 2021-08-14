17cfa4a0aad15b7ffee3190f800db417a18d7518
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-14 21:54:14 +0900

    Expansion of phi_{12, 1}(x) where phi_{r, s}(x) = Sum_{n, m>0} m^r * n^s * x^{m*n}.

---
+++ 282/282548/282548_01.rb
@@ -0,0 +1,29 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+def sigma(x, i)
+  return i ** x if i < 2
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def A(k, n)
+  (0..n).map{|i| i * sigma(k, i)}
+end
+
+n = 10100
+ary = A(11, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
