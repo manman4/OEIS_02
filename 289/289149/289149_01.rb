3e000f9b1c5268bf90d2037ca8f9dcaf4f311ea3
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-16 00:31:29 +0900

    Coefficients of the modular function j_6 = j^6 - 4464*j^5 + 7121736*j^4 - 4850017536*j^3 + 1304194222980*j^2 - 96687754014528*j + 453039686271072.

---
+++ 289/289149/289149_01.rb
@@ -0,0 +1,98 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# m���ȉ������o��
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A001158(n)
+  s = 0
+  (1..n).each{|i| s += i * i * i if n % i == 0}
+  s
+end
+
+def A004009(n)
+  a = [1] + (1..n).map{|i| 240 * A001158(i)}
+end
+
+def E4_3(n)
+  ary = A004009(n)
+  power(ary, 3, n)
+end
+
+def s(n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - k * a[j] * ary[-j]} / i}
+  ary
+end
+
+def f(n)
+  a = E4_3(n)
+  b = A(-24, n)
+  mul(a, b, n)
+end
+
+def A289141(n)
+  f_ary0 = f(n)
+  f_ary = f_ary0
+  b = [[1], f_ary]
+  (2..n).each{|i|
+    b_ary = mul(f_ary, f_ary0, n)
+    f_ary = b_ary
+    b << f_ary
+  }
+  c = [1]
+  (1..n).each{|i|
+    ci = f_ary[i]
+    c << - ci
+    (i..n).each{|j|
+      f_ary[j] -= ci * b[n - i][j - i]
+    }
+  }
+  c.reverse
+end
+
+def J_k(k, n)
+  a = f(n + k)
+  j_ary = [[0] * k + [1] + Array.new(n, 0), [0] * (k - 1) + a[0..-k]]
+  f_ary = a
+  (2..k).each{|i|
+    b_ary = mul(f_ary, a, n + k)
+    f_ary = b_ary
+    j_ary << [0] * (k - i) + f_ary[0..-k + i - 1]
+  }
+  c_ary = A289141(k)
+  (-k).upto(n).map{|i| (0..k).inject(0){|s, j| s + c_ary[j] * j_ary[j][i + k]}}
+end
+
+k = 6
+n = 7
+ary = J_k(k, n)
+(-k).upto(n){|i|
+  j = ary[i + k]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
