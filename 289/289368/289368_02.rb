87cb8613155c9c1cc39f45ea74a951d977b3e6fb
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-24 01:07:45 +0900

    Coefficients in expansion of (E_6^2/E_4^3)^(1/24).

---
+++ 289/289368/289368_02.rb
@@ -0,0 +1,118 @@
+require 'prime'
+
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+def E_2k(k, n)
+  a = (-4 * k / bernoulli(2 * k)[-1])
+  b = a.denominator
+  c = a.numerator
+  [b] + (1..n).map{|i| c * sigma(2 * k - 1, i)}
+end
+
+def A008683(n)
+  ary = n.prime_division
+  return (-1) ** (ary.size % 2) if ary.all?{|i| i[1] == 1}
+  0
+end
+
+def A(k, n)
+  a = mul(I(E_2k(k, n), n), E_2k(k + 1, n), n)
+  m_ary = [0] + (1..n).map{|i| A008683(i)}
+  ary = []
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += m_ary[i / j] * a[j] if i % j == 0
+    }
+    ary << s / i
+  }
+  ary
+end
+
+def A289367(n)
+  ary3 = [0] + A(3, n)
+  ary2 = [0] + A(2, n)
+  (0..n).map{|i| (ary3[i] - ary2[i]) / 288}
+end
+
+def s(n, ary)
+  s = 0
+  (1..n).each{|i| s -= i * ary[i] if n % i == 0}
+  s
+end
+
+def B(k, n)
+  ary = [1]
+  f_ary = A289367(n)
+  s_ary = [0] + (1..n).map{|i| s(i, f_ary)}
+  (1..n).each{|i|
+    ary << k * (1..i).inject(0){|s, j| s + s_ary[j] * ary[-j]} / i
+  }
+  ary
+end
+
+k = 12
+n = 11
+ary = B(k, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
