49513ac604aecce54d2f04716501cd223bca3366
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-23 21:56:41 +0900

    Fourier coefficients of -q*(Delta/q)' where Delta is the normalized unique weight-twelve cusp form for the full modular group (the generating function of Ramanujan's tau function).

---
+++ 289/289833/289833_01.rb
@@ -0,0 +1,32 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+n = 26
+a = A([[1, 24]], n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts -i * a[i]
+}
