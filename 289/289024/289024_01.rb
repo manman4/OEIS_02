1785800d826a1b09434d398f6918815abe2fe2d7
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-23 22:16:14 +0900

    Exponents a(1), a(2), ... such that E_10, 1 - 264*q - 135432*q^2 + ... (A013974) is equal to (1-q)^a(1) (1-q^2)^a(2) (1-q^3)^a(3) ... .

---
+++ 289/289024/289024_01.rb
@@ -0,0 +1,107 @@
+require 'prime'
+
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def E_2k(k, n)
+  a = (-4 * k / bernoulli(2 * k)[-1])
+  b = a.denominator
+  c = a.numerator
+  [b] + (1..n).map{|i| c * sigma(2 * k - 1, i)}
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+def A008683(n)
+  ary = n.prime_division
+  return (-1) ** (ary.size % 2) if ary.all?{|i| i[1] == 1}
+  0
+end
+
+def A110163(n)
+  a = I(E_2k(2, n), n)
+  b = E_2k(3, n)
+  c = mul(a, b, n)
+  m_ary = [0] + (1..n).map{|i| A008683(i)}
+  ary = []
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += m_ary[i / j] * c[j] if i % j == 0
+    }
+    ary << 8 + s / (3 * i)
+  }
+  ary
+end
+
+def A288851(n)
+  a = I(E_2k(3, n), n)
+  b = E_2k(4, n)
+  c = mul(a, b, n)
+  m_ary = [0] + (1..n).map{|i| A008683(i)}
+  ary = []
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += m_ary[i / j] * c[j] if i % j == 0
+    }
+    ary << 12 + s / (2 * i)
+  }
+  ary
+end
+
+n = 12
+ary4 = A110163(n)
+ary6 = A288851(n)
+ary = (1..n).map{|i| ary4[i - 1] + ary6[i - 1]}
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
