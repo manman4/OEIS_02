844e36751c6d49543ee712b437acd3ec8483cac4
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-23 23:07:44 +0900

    Coefficients in expansion of E_2^(1/2).

---
+++ 289/289291/289291_01.rb
@@ -0,0 +1,61 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def E_2k(k, n)
+  a = -4 * k / bernoulli(2 * k)[-1]
+  b = a.denominator
+  c = a.numerator
+  [b] + (1..n).map{|i| c * sigma(2 * k - 1, i)}
+end
+
+# ary[0] = 1, ary[1] �͋���
+def sqrt_a(ary)
+  n = ary.size - 1
+  a = [1, ary[1] / 2]
+  i = 1
+  while i < n
+    a << (ary[i + 1] - (1..i).inject(0){|s, j| s + a[j] * a[-j]}) / 2
+    i += 1
+  end
+  a
+end
+
+def A(k, n)
+  sqrt_a(E_2k(k, n))
+end
+
+n = 16
+ary = A(1, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
