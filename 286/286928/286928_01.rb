aecb15d30752d7c274422186479fa9985e3aa29e
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-25 00:10:21 +0900

    Number of solutions to the equation x_1 + ... + x_n =0 satisfying -n<=x_i<=n (1<=i<=n).

---
+++ 286/286928/286928_01.rb
@@ -0,0 +1,34 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# m���ȉ������o��
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(n)
+  ary = Array.new(2 * n + 1, 1)
+  power(ary, n, n * n)[-1]
+end
+
+n = 18
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
