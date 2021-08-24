8a1b171c17324bbc10d63715328aa18464ac9994
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-25 00:03:16 +0900

    Digits of one of the two 13-adic integers sqrt(-1) (digits 0, 1, ... , 9, 10, 11, 12 are used instead of 0, 1, ... , 9, A, B, C).

---
+++ 286/286838/286838_01.rb
@@ -0,0 +1,26 @@
+def A(k, m, n)
+  d_ary = []
+  ary = [0]
+  a, mod = k, m
+  (n + 1).times{|i|
+    b = a % mod
+    d_ary << (b - ary[-1]) / m ** i
+    ary << b
+    a = b ** m
+    mod *= m
+  }
+  d_ary
+end
+def A286838(n)
+  A(5, 13, n)
+end
+
+n = 10000
+ary = A286838(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
