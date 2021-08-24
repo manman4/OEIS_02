e2d925ac06ad2121f4c127fe9d58cd5a5e913a2a
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-25 00:34:24 +0900

    Expansion of 1 / ((1-x)^2*(1-x^2)*(1-x^3)*...*(1-x^9)).

---
+++ 288/288344/288344_01.rb
@@ -0,0 +1,19 @@
+def A(k, n)
+  ary = [1] + (1..k).to_a
+  ps = Array.new(n + 1, 0)
+  ps[0] = 1
+  ary.each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  ps
+end
+
+n = 10000
+ary = A(9, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
