35538f9b4761b5a22849d2ad46e81aee1b6341a2
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-24 23:33:16 +0900

    Square array A(n,k), n>=0, k>=0, read by antidiagonals, where column k is the expansion of Product_{j>=1} (1 - x^j)^k.

---
+++ 286/286354/286354_01.rb
@@ -0,0 +1,29 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(1, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - k * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 12
+a = []
+(0..n).each{|i| a << A(i, n - i)}
+ary = []
+(0..n).each{|i|
+  (0..i).each{|j|
+    ary << a[i - j][j]
+  }
+}
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
