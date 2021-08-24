4586bc02439d151b53d6aac2317390b76d08f1c0
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-24 12:22:41 +0900

    Coefficients in expansion of -q*E'_14/E_14 where E_14 is the Eisenstein Series (A058550).

---
+++ 289/289640/289640_01.rb
@@ -0,0 +1,57 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def bernoulli(n)
+  ary = []
+  a = []
+  (0..n).each{|i|
+    a << 1r / (i + 1)
+    i.downto(1){|j| a[j - 1] = j * (a[j - 1] - a[j])}
+    ary << a[0] # Bn = a[0]
+  }
+  ary
+end
+
+def E_2k(k, n)
+  a = -4 * k / bernoulli(2 * k)[-1]
+  b = a.denominator
+  c = a.numerator
+  [b] + (1..n).map{|i| c * sigma(2 * k - 1, i)}
+end
+
+# ary[0]���v�Z�̓s�����肭���p
+def A(k, n)
+  ary = [1]
+  a = E_2k(k, n)
+  (1..n).each{|i|
+    ary << (1..i).inject(0){|s, j| s - a[j] * ary[-j]}
+    ary[0] += 1
+  }
+  ary
+end
+
+k = 7
+n = 11
+ary = A(k, n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
