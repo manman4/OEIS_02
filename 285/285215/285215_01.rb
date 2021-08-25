0fd0ddf0bd31204adb374b08f5ca0a0287ab2814
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-25 21:14:43 +0900

    Expansion of Product_{k>=1} (1 - x^(4*k))^(4*k) / (1 - x^k)^k.

---
+++ 285/285215/285215_01.rb
@@ -0,0 +1,21 @@
+def s(k, m, n)
+  s = 0
+  (1..n).each{|i| s += i * i if n % i == 0 && i % k == m}
+  s
+end
+def A(k, n)
+  ary = [1]
+  s_ary = [0] + (1..n).map{|i| s(k, 0, i) - s(1, 0, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - ary[-j] * s_ary[j]} / i}
+  ary
+end
+
+n = 38
+ary = A(4, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
