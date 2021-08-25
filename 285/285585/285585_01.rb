b6703ff8f85347c4048147dfd83b76bfb6dea393
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-25 22:27:07 +0900

    Expansion of r(q^5) / r(q)^5 in powers of q where r() is the Rogers-Ramanujan continued fraction.

---
+++ 285/285585/285585_01.rb
@@ -0,0 +1,41 @@
+def s(k, m, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == m}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(5, 1, i) + s(5, 4, i) - s(5, 2, i) - s(5, 3, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - a[j] * ary[-j]} / i}
+  a_ary = Array.new(n + 1, 0)
+  (0..n / k).each{|i| a_ary[k * i] = ary[i]}
+  a_ary
+end
+
+def B(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(5, 1, i) + s(5, 4, i) - s(5, 2, i) - s(5, 3, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + k * a[j] * ary[-j]} / i}
+  ary
+end
+
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+n = 42
+ary = mul(A(5, n), B(5, n), n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
