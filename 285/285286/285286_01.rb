33f0defdd28766ef020ce99cf5f501ce0361d14a
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-25 21:33:29 +0900

    Expansion of Product_{k>=0} 1/(1 + x^(3*k+1))^(3*k+1).

---
+++ 285/285286/285286_01.rb
@@ -0,0 +1,22 @@
+def s(k, m, n)
+  s = 0
+  (1..n).each{|i| s += (-1) ** (n / i + 1) * i * i if n % i == 0 && i % k == m}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  s_ary = [0] + (1..n).map{|i| s(k, 1, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - ary[-j] * s_ary[j]} / i}
+  ary
+end
+
+n = 45
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
