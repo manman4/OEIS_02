f0479232b6d20a2a73db3e3e9ae8c2fda9464b5e
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-27 23:15:07 +0900

    Expansion of exp( Sum_{n>=1} sigma(8*n)*x^n/n ) in powers of x.

---
+++ 283/283120/283120_02.rb
@@ -0,0 +1,34 @@
+require 'prime'
+def power(a, n)
+  return 1 if n == 0
+  k = power(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  if x == 0
+    pq.each{|a, n| sum *= n + 1}
+  else
+    pq.each{|a, n| sum *= (power(a, (n + 1) * x) - 1) / (power(a, x) - 1)}
+  end
+  sum
+end
+def A(k, m, n)
+  ary = [1]
+  s_ary = [0] + (1..n).map{|i| sigma(k, i * m)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + ary[-j] * s_ary[j]} / i}
+  ary
+end
+
+n = 24
+ary = A(1, 8, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
