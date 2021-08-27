8b32cfc30e1e0b5f4fb7debe1751a7d8a00095a0
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-27 17:43:52 +0900

    Number of solutions to x^5 + y^5 = z^5 mod n.

---
+++ 288/288100/288100_01.rb
@@ -0,0 +1,23 @@
+def A(k, n)
+  a = []
+  b = Array.new(n, 0)
+  (0..n - 1).each{|i|
+    j = i ** k % n
+    a[i] = j
+    b[j] += 1
+  }
+  s = 0
+  (0..n - 1).each{|i|
+    (0..n - 1).each{|j|
+      s += b[(a[i] + a[j]) % n]
+    }
+  }
+  s
+end
+
+n = 1000
+(1..n).each{|i|
+  print i
+  print ' '
+  puts A(5, i)
+}
