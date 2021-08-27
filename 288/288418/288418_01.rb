01901857fe44d9c763231ba62bf13ec8b7a80307
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-27 16:43:50 +0900

    a(n) = Sum_{d|n} d^2*A000593(n/d).

---
+++ 288/288418/288418_01.rb
@@ -0,0 +1,18 @@
+def s0(n)
+  s = 0
+  1.step(n, 2){|i| s += i if n % i == 0}
+  s
+end
+
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i ** k * s0(n / i) if n % i == 0}
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  print i
+  print ' '
+  puts s(2, i)
+}
