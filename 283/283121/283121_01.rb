92de0aed876a92bb0fca3c5fa589ac0c9800b8f1
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-27 23:16:17 +0900

    Expansion of exp( Sum_{n>=1} sigma(9*n)*x^n/n ) in powers of x.

---
+++ 283/283121/283121_01.rb
@@ -0,0 +1,72 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def p0(n)
+  (3 * n * n - n) / 2
+end
+
+def p1(n)
+  (3 * n * n + n) / 2
+end
+
+def A010815(n)
+  ary = Array.new(n + 1, 0)
+  ary[0] = 1
+  i = 1
+  j = p0(i)
+  while j <= n
+    ary[j] = (-1) ** i
+    i += 1
+    j = p0(i)
+  end
+  i = 1
+  j = p1(i)
+  while j <= n
+    ary[j] = (-1) ** i
+    i += 1
+    j = p1(i)
+  end
+  ary
+end
+
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A283121(n)
+  ary = A010815(n)
+  ary3 = Array.new(n + 1, 0)
+  (0..n / 3).each{|i| ary3[3 * i] = ary[i]}
+  ary34 = power(ary3, 4, n)
+  ps = Array.new(n + 1){0}
+  ps[0] = 1
+  (1..n).each{|num|
+    (num..n).each{|i|
+      ps[i] += ps[i - num]
+    }
+  }
+  ary113 = power(ps, 13, n)
+  mul(ary34, ary113, n)
+end
+
+n = 1100
+ary = A283121(n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
