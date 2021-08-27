d31f69fa2f6a9431709e151346def4fbcfa2f667
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-27 23:44:46 +0900

    a(n) = sigma_2(3*n).

---
+++ 283/283237/283237_01.rb
@@ -0,0 +1,24 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+n = 10000
+(1..n).each{|i|
+  print i
+  print ' '
+  puts sigma(2, i * 3)
+}
