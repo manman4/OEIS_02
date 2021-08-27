0383bff061f1824351bbfe043cd3725f20f1b4eb
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-27 17:38:25 +0900

    Expansion of 1/Product_{j>=1} Product_{i>=1} (1 + x^(i*j)).

---
+++ 288/288007/288007_01.rb
@@ -0,0 +1,20 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  p a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+n = 70
+p ary = A(1.step(n, 2).map{|i| [i, 1]}, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
