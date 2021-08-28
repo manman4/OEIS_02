5c4fe238d21aac35d2b47fac00bcbe13e99f6f34
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-28 22:09:27 +0900

    Denominators of poly-Bernoulli numbers B_n^(k) with k = 9.

---
+++ 283/283935/283935_01.rb
@@ -0,0 +1,29 @@
+n = 11
+@ary = [[1], [0, 1]]
+a = [0, 1]
+(2..n).each{|i|
+  b = Array.new(i + 1, 0)
+  (1..i - 1).each{|j|
+    b[j] = a[j - 1] + j * a[j]
+  }
+  b[i] = 1
+  a = b
+  @ary << a
+}
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def b(n, k)
+  (-1) ** (n % 2) * (0..n).inject(0){|s, i| s + (-1) ** (i % 2) * f(i) * @ary[n][i] / (i + 1r) ** k}
+end
+
+(0..n).each{|i|
+  j = b(i, 9).denominator
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
