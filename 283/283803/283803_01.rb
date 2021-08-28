348adb4d0e99897b97351120afe4fce8681e00bf
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-28 21:42:10 +0900

    Expansion of exp( Sum_{n>=1} -A283369(n)/n*x^n ) in powers of x.

---
+++ 283/283803/283803_01.rb
@@ -0,0 +1,31 @@
+# Cf. Product_{k>=1} (1 - x^k)^(k^(m*k)): 
+#     A010815 (m=0), 
+#     A283499 (m=1), 
+#     A283534 (m=2), 
+#     A283536 (m=3), 
+#     A283803 (m=4).
+
+def s(k, i)
+  s = 0
+  (1..i).each{|j| s += j ** (k * j + 1) if i % j == 0}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  s_ary = [0] + (1..n).map{|i| s(k, i)}
+  n.times{|i|
+    ary << (1..i + 1).inject(0){|s, j| s - ary[-j] * s_ary[j]} / (i + 1)
+  }
+  ary
+end
+
+n = 10
+ary = A(4, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
