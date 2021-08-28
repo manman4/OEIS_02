8df9850712e9e1bf32a789ee5cd45870d99534dd
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-28 21:09:49 +0900

    Expansion of exp( Sum_{n>=1} -A283498(n)/n*x^n ) in powers of x.

--- 283/283499/283499_01.rb
+++ 283/283499/283499_01.rb
@@ -1,10 +1,8 @@
-def power(a, n)
-  return 1 if n == 0
-  k = power(a, n >> 1)
-  k *= k
-  return k if n & 1 == 0
-  return k * a
-end
+# Cf. Product_{k>=1} (1 - x^k)^(k^(m*k)): 
+#     A010815 (m=0), 
+#     A283499 (m=1), 
+#     A283534 (m=2), 
+#     A283536 (m=3).
 
 def s(k, i)
   s = 0
