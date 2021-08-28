97ed5ca8aaac8f425c9f53c6eb16e760bcb39f18
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-28 20:43:31 +0900

    a(n) = Sum_{d|n} d^(3*d + 1).

---
+++ 283/283535/283535_01.rb
@@ -0,0 +1,14 @@
+def s(k, i)
+  s = 0
+  (1..i).each{|j| s += j ** (k * j + 1) if i % j == 0}
+  s
+end
+
+n = 12
+(1..n).each{|i|
+  j = s(3, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
