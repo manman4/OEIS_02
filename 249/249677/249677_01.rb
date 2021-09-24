4897c8ca0fe25df2a6d2abc7da7795219bcd6840
manman4 <7023566+manman4@users.noreply.github.com>
2021-09-25 04:35:40 +0900

    Triangle, read by rows, with row n forming the coefficients in Product_{k=0..n} (1 + k^3*x).

---
+++ 249/249677/249677_01.rb
@@ -0,0 +1,31 @@
+# m次以下を取り出す
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def A(n)
+  c_ary = [1]
+  ary = [[1]]
+  (1..n).each{|i|
+    c_ary = mul(c_ary, [1, i * i * i], i)
+    ary << c_ary
+  }
+  ary.flatten
+end
+
+n = 139
+ary = A(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
