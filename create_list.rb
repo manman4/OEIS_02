b1017473851f2c240b6ec4bb3e6192500256b91d
manman4 <7023566+manman4@users.noreply.github.com>
2021-10-23 22:48:03 +0900

    リポジトリで保存している数列

--- create_list.rb
+++ create_list.rb
@@ -4,18 +4,23 @@ require 'find'
 path = ARGV[0] ? ARGV[0] : '.'
 # .txtはgitignoreしているので.rbファイルに保存
 cnt = 0
+ary = []
 File.open("squence_list.rb", mode = "w"){|f|
   f.write("#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} 現在\n")
   Find.find(path){|item|
     # ./git とか省く
     if /[0-9]{3}/.match?(File.dirname(item)[2..4])
-      # ディレクトリのみ対象
-      if File.ftype(item) == "directory"
+      # ファイルのみ対象
+      # ディレクトリ対象だと空に見えても.empty?でfalseになったため
+      if File.ftype(item) == "file"
         base = File.basename(item)
-        # .vscodeとか除くため
-        if /[0-9]{6}/.match?(base)
-          cnt += 1
-          f.write("#{cnt} #{base}\n")
+        sequence_num = base[0..5]
+        if /[0-9]{6}/.match?(sequence_num)
+          if !ary.include?(sequence_num)
+            cnt += 1
+            ary << sequence_num
+            f.write("#{cnt} #{sequence_num}\n")
+          end
         end
       end
     end
