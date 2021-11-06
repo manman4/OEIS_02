26271cbc5076034b53b0ab78390dd462548d1481
manman4 <7023566+manman4@users.noreply.github.com>
2021-11-06 21:15:48 +0900

    a(n) = Sum_{k=0..n}(binomial(n,k)*binomial(n+k-1,n-k)).

---
+++ 262/262440/262440_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  (0..n).inject(0){|s, i| s + ncr(n, i) * ncr(n + i - 1, n - i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
