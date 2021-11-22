85b520b49310f65dd8b7195f2c15d5888f45e835
manman4 <7023566+manman4@users.noreply.github.com>
2021-11-22 12:18:02 +0900

    E.g.f. satisfies: A(x) = 1/(1-x)^(A(x)^3).

--- 264/264408/264408_01.rb
+++ 264/264408/264408_01.rb
@@ -2,20 +2,20 @@ def A(k, n)
   # aは符号なし
   a = [1]
   ary = [1]
-  n.times{|i|
+  (1..n).each{|i|
     a << 0
     b = [0]
-    (0..i).each{|j|
-      b[j + 1] = a[j] + i * a[j + 1]
+    (0..i - 1).each{|j|
+      b[j + 1] = a[j] + (i - 1) * a[j + 1]
     }
     a = b
-    ary << (0..i + 1).inject(0){|s, j| s + (k * j + 1) ** (j - 1) * a[j]}
+    ary << (0..i).inject(0){|s, j| s + (k * j + 1) ** (j - 1) * a[j]}
   }
   ary
 end
 
-n = 500
-ary = A(3, n)
+n = 18
+p ary = A(3, n)
 (0..n).each{|i|
   j = ary[i]
   break if j.to_s.size > 1000
