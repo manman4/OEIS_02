17de4b018d4fe1eee3fe108e1438b5ca2f1eb290
manman4 <7023566+manman4@users.noreply.github.com>
2022-01-05 18:03:22 +0900

    Coefficients in the expansion Product_{ n>=1 } (1-q^(2n-1))^2/(1-q^(2n))^2.

---
+++ 274/274621/274621_01.rb
@@ -0,0 +1,20 @@
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(k, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| 2 * s(2, i) - s(1, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - k * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 10000
+ary = A(-2, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
