420217eb7958afee2cd5d0ea53e0d64a2dbcf1ad
manman4 <7023566+manman4@users.noreply.github.com>
2022-01-16 17:48:00 +0900

    a(0)=1; thereafter a(n) = -3*Sum_{k=1..n} binomial(2n,2k)*a(n-k).

---
+++ 210/210676/210676_01.rb
@@ -0,0 +1,22 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (0..i - 1).inject(0){|s, j| s + k * ncr(2 * i, 2 * j) * ary[j]}
+  }
+  ary
+end
+
+n = 200
+ary = A(-3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
