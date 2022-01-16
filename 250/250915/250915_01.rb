2d36f9ab31e4eb7f59b86e0ddb3b0a91d8f5805a
manman4 <7023566+manman4@users.noreply.github.com>
2022-01-16 21:06:42 +0900

    E.g.f.: (32 - 31*cosh(x)) / (41 - 40*cosh(x)).

---
+++ 250/250915/250915_01.rb
@@ -0,0 +1,22 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (1..i - 1).inject(1){|s, j| s + k * ncr(i, j) * ary[j]}
+  }
+  ary
+end
+
+n = 200
+ary = A(4, 2 * n)
+(0..n).each{|i|
+  j = ary[2 * i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
