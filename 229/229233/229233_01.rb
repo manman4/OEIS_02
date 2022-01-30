6706ea702048b9a6c5712c1eeb658927387f3693
manman4 <7023566+manman4@users.noreply.github.com>
2022-01-30 23:56:35 +0900

    O.g.f.: Sum_{n>=0} x^n / Product_{k=1..n} (1 - n*k*x).

---
+++ 229/229233/229233_01.rb
@@ -0,0 +1,24 @@
+def A(n)
+  a = [1]
+  ary = [1]
+  (1..n).each{|i|
+    a << 0
+    b = [0]
+    (0..i - 1).each{|j|
+      b[j + 1] = a[j] + (j + 1) * a[j + 1]
+    }
+    a = b
+    ary << (0..i).inject(0){|s, j| s + j ** (i - j) * a[j]}
+  }
+  ary
+end
+
+n = 400
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
