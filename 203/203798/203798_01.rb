46afe0fe2ba8c291a9e50844932640c6fd74bb89
manman4 <7023566+manman4@users.noreply.github.com>
2022-02-01 00:18:59 +0900

    E.g.f.: Sum_{n>=0} (1 - exp(-n^3*x))^n.

---
+++ 203/203798/203798_01.rb
@@ -0,0 +1,29 @@
+def f(n)
+  return 1 if n < 2
+  return (1..n).inject(:*)
+end
+
+def A(n)
+  a = [1]
+  ary = [1]
+  (1..n).each{|i|
+    a << 0
+    b = [0]
+    (0..i - 1).each{|j|
+      b[j + 1] = a[j] + (j + 1) * a[j + 1]
+    }
+    a = b
+    ary << (0..i).inject(0){|s, j| s + (-1) ** (i - j) * j ** (3 * i) * f(j) * a[j]}
+  }
+  ary
+end
+
+n = 200
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
