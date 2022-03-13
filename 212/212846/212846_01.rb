6894e29bcddd3a5b633ba21ae28a65dbcf09b87e
manman4 <7023566+manman4@users.noreply.github.com>
2022-03-13 23:10:46 +0900

    Polylogarithm li(-n,-1/2) multiplied by (3^(n+1))/2.

---
+++ 212/212846/212846_01.rb
@@ -0,0 +1,29 @@
+def f(n)
+  return 1 if n < 2
+  return (1..n).inject(:*)
+end
+
+def A(p, q, n)
+  a = [1]
+  ary = [1]
+  (1..n).each{|i|
+    a << 0
+    b = [0]
+    (0..i - 1).each{|j|
+      b[j + 1] = a[j] + (j + 1) * a[j + 1]
+    }
+    a = b
+    ary << (0..i).inject(0){|s, j| s + p ** j * q ** (i - j) * f(j) * a[j]}
+  }
+  ary
+end
+
+n = 500
+ary = A(-1, 3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
