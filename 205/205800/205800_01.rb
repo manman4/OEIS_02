d330a7139fe977bf642385f083ef3119d24fd3db
manman4 <7023566+manman4@users.noreply.github.com>
2022-04-29 21:02:27 +0900

    Expansion of e.g.f. exp( Sum_{n>=1} x^(n^2) ).

---
+++ 205/205800/205800_01.rb
@@ -0,0 +1,28 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n)
+  ary = [1]
+  (1..n).each{|i|
+    m = f(i - 1)
+    s = 0
+    (1..i).each{|j|
+      break if j * j > i
+      s += j * j * ary[i - j * j] * m / f(i - j * j)
+    }
+    ary << s
+  }
+  ary
+end
+
+n = 500
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
