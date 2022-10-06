6f070dc89a0fc2c6484f9b5d26a8886c9b36342c
manman4 <7023566+manman4@users.noreply.github.com>
2022-10-07 01:00:16 +0900

    Stirling transform of A077957 (aerated powers of 2).

---
+++ 264/264036/264036_01.rb
@@ -0,0 +1,25 @@
+def A(k, n)
+  a = [1]
+  ary = [1]
+  (1..n).each{|i|
+    a << 0
+    b = [0]
+    (0..i - 1).each{|j|
+      b[j + 1] = a[j] + (j + 1) * a[j + 1]
+    }
+    a = b
+    # i > 0 のときa[0] = 0
+    ary << (0..i / 2).inject(0){|s, j| s + k ** j * a[2 * j]}
+  }
+  ary
+end
+
+n = 700
+ary = A(2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
