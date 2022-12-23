1322e51b151a13e66cc1df236f7d54c569fb3c5e
manman4 <7023566+manman4@users.noreply.github.com>
2022-12-23 22:56:42 +0900

    a(n) = Sum_{k>=0} floor(n/(5*k + 2)).

---
+++ 218/218445/218445_01.rb
@@ -0,0 +1,18 @@
+def A001877(n)
+  return 0 if n == 0
+  s = 0
+  (1..n).each{|i|
+    s += 1 if n % i == 0 && i % 5 == 2
+  }
+  s
+end
+
+n = 10000
+s = 0
+(0..n).each{|i| 
+  s += A001877(i)
+  break if s.to_s.size > 1000
+  print i
+  print ' '
+  puts s
+}
