e8529478fe94da65ed6ef7d09cd8ddb88db774da
manman4 <7023566+manman4@users.noreply.github.com>
2023-01-18 16:12:11 +0900

    Expansion of Product_{k>=0} (1 - x^(7*k+4)) in powers of x.

---
+++ 284/284502/284502_01.rb
@@ -0,0 +1,27 @@
+def A(k, m, n)
+  s = 0
+  (1..n).each{|i|
+    s += i if n % i == 0 && i % k == m
+  }
+  s
+end
+
+def B(k, m, n)
+  (1..n).map{|i| A(k, m, i)}
+end
+
+def C(k, m, n)
+  ary = [1]
+  a = [0] + B(k, m, n)
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 10100
+m = 10000
+ary = C(7, 4, n)
+(0..m).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
