20a8b4fbcdc40bbbf0d7545ce5f4d0efad9d6478
manman4 <7023566+manman4@users.noreply.github.com>
2023-07-19 22:11:30 +0900

    G.f. satisfies A(x) = (1 + x*A(x)) * (1 + x*A(x)^4).

---
+++ 215/215623/215623_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, m, n)
+  (0..n).inject(0){|s, i| s + ncr(k * n + m * i + 1, i) * ncr(k * n + m * i + 1, n - i) / (k * n + m * i + 1).to_r}.to_i
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(1, 3, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
