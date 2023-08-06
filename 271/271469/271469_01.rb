ddbabceb276a2ef173a18da88d80f0cef63e707f
manman4 <7023566+manman4@users.noreply.github.com>
2023-08-06 20:38:15 +0900

    G.f. satisfies A(x) = 1 + x*(A(x)^3 - A(x)^4 + A(x)^5).

---
+++ 271/271469/271469_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, m, n)
+  return 1 if n == 0
+  (0..n - 1).inject(0){|s, i| s + ncr(n, i) * ncr(k * n + m * i, n - 1 - i)} / n
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(3, 1, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
