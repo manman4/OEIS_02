da9fcd900b71c963282ac83e425931ba55ca352c
manman4 <7023566+manman4@users.noreply.github.com>
2024-01-28 20:38:51 +0900

    G.f. satisfies A(x) = (1 + x*A(x))^3 * (1 + x*A(x)^2).

---
+++ 274/274735/274735_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = (1/(n+1)) * Sum_{k=0..n} binomial(n+k,k) * binomial(3*n+3*k+3,n-k).
+def A(n)
+  (0..n).inject(0){|s, i| s + ncr(n + i, i) * ncr(3 * n + 3 * i + 3, n - i)} / (n + 1)
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
