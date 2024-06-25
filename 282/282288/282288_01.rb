45b3da292895bdd9d50a0e70341208bde30ac87a
manman4 <7023566+manman4@users.noreply.github.com>
2024-06-26 02:05:06 +0900

    Expansion of (Sum_{k>=0} x^(k^4))^19.

---
+++ 282/282288/282288_01.rb
@@ -0,0 +1,44 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# m���ȉ������o��
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(k, n)
+  ary = Array.new(n + 1, 0)
+  i = 0
+  j = i * i * i * i
+  while j <= n
+    ary[j] += 1
+    i += 1
+    j = i * i * i * i
+  end
+  ary
+  power(ary, k, n)
+end
+
+n = 10100
+m = 10000
+ary = A(19, n)
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
