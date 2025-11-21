aff6e814c0845e0edb5be9038a9bda08686d5b9e
manman4 <7023566+manman4@users.noreply.github.com>
2025-11-21 20:06:18 +0900

    E.g.f. A(x) satisfies A(x) = 1 + A(x)^4 * Integral 1/A(x)^4 dx.

---
+++ 234/234294/234294_01.rb
@@ -0,0 +1,23 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(0) = 1; a(n) = a(n-1) + 3 * Sum_{k=1..n-1} binomial(n-1,k) * a(k) * a(n-k).
+def a(n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (1..i - 1).inject(ary[-1]){|s, k| s + 3 * ncr(i - 1, k) * ary[k] * ary[i - k]}
+  }
+  ary
+end
+
+n = 500
+ary = a(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
