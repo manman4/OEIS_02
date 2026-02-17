d4c82c183c670fe1a87564a5da3151c8edfcd049
manman4 <7023566+manman4@users.noreply.github.com>
2026-02-17 21:15:51 +0900

    Number of partitions of n having (sum of odd parts) < (sum of even parts).

---
+++ 239/239259/239259_01.rb
@@ -0,0 +1,39 @@
+# m>0
+
+def s(f_ary, g_ary, n, m)
+  s = 0
+  (m..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(ary, n, m = 1)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + s(j[0], j[1], i, m)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s + a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+# a(2*n+m) = Sum_{k=0..floor(n-1)/2} A000009(2*k+m) * A000041(n-k) for m=0, 1.
+def a(n)
+  ary = []
+  ary1 = Array.new(n + 1, 1)
+  ary2 = Array.new(n + 1, -1)
+  a_odd = A([[ary2, ary2]], n)
+  a_all = A([[ary1, ary1]], n)
+  (0..n).each{|i|
+    m = i % 2
+    j = i / 2
+    ary << (0..(j - 1) / 2).inject(0){|s, k| s + a_odd[2 * k + m] * a_all[j - k]}
+  }
+  ary
+end
+
+n = 10000
+ary = a(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
