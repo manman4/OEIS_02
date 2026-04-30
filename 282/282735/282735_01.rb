b08b82b8b238bdfd7777c032908e686c7fcbe259
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-30 20:02:12 +0900

    Number of triangulations of a convex n-gon in the plane each of whose sides is subdivided by 2 points.

--- 282/282735/282735_01.rb
+++ 282/282735/282735_01.rb
@@ -8,7 +8,7 @@ def a(n)
   (0..3 * n - 2).inject(0){|s, i| s + ncr(3 * n - 4, i) * (0..(3 * n - i - 2) / 4).inject(0){|t, j| t + (-1) ** j * ncr(n, j) * ncr(4 * n - i - 4 - 4 * j, n - 2)}}
 end
 
-n = 1001
+n = 101
 (2..n).each{|i|
   j = a(i)
   break if j.to_s.size > 1000
