2cb53d9a7a524376a7a94b9faefdff961a7043ee
manman4 <7023566+manman4@users.noreply.github.com>
2026-08-22 13:29:18 +0900

    Smallest number expressible in the form a^2 + 2*b^2, with positive integers a and b, in exactly n ways.

---
+++ 200/200977/200977_01.rb
@@ -0,0 +1,21 @@
+require 'prime'
+
+def A(n)
+  m = Math.sqrt(n).to_i + 1
+  ary = Array.new(n + 1, 0)
+  (1..m).each{|i|
+    (1..m).each{|j|
+      k = i * i + 2 * j * j
+      if k <= n
+        ary[k] += 1
+      end
+    }
+  }
+  ary
+end
+
+n = 10 ** 7
+ary = A(n)
+
+p a = (0..100).map{|i| ary.index(i)}
+
